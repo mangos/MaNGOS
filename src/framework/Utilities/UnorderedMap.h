e3c5655907d7c15c1213b9a74ca712dd11f0a128
Wyk3d <Wyk3d@getmangos.com>
2008-11-08 22:03:17 +0200

    [6808] Fixed VC9 build when TR1 is not present and added revision number output to the exception report

--- src/framework/Utilities/UnorderedMap.h
+++ src/framework/Utilities/UnorderedMap.h
@@ -28,7 +28,7 @@
 #include <tr1/unordered_map>
 #elif COMPILER == COMPILER_GNU && __GNUC__ >= 3
 #include <ext/hash_map>
-#elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1500    // VC9.0 and later
+#elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1500 && _HAS_TR1   // VC9.0 SP1 and later
 #include <unordered_map>
 #else
 #include <hash_map>
@@ -37,7 +37,7 @@
 #ifdef _STLPORT_VERSION
 #define UNORDERED_MAP std::hash_map
 using std::hash_map;
-#elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1500
+#elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1500 && _HAS_TR1
 #define UNORDERED_MAP std::tr1::unordered_map
 #elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1300
 #define UNORDERED_MAP stdext::hash_map
