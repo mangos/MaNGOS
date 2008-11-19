fa3fe5e9ede97ca1be1e3a41b6b6e4aac5f36973
Blaymoira <handras@emitelnet.hu>
2008-11-19 19:18:46 +0100

    SpellEffect Fix for 40802, 43723 (cherry picked from commit 90ec5bed9a29a06190c72851a6bd6e57c3b9bdbc)
    Signed-off-by: Blaymoira <handras@emitelnet.hu>
    
    40802: use exiting function instead code duplication.
    43723: added in commented state (impossible propertly fix without hacks until targeting mode 7 implementation)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -608,7 +608,7 @@ void Spell::EffectDummy(uint32 i)
     switch(m_spellInfo->SpellFamilyName)
     {
         case SPELLFAMILY_GENERIC:
-            // Gnomish Poultryizer trinket
+        {
             switch(m_spellInfo->Id )
             {
                 case 8063:                                  // Deviate Fish
@@ -992,6 +992,50 @@ void Spell::EffectDummy(uint32 i)
                     if(unitTarget)
                         m_caster->CastSpell(unitTarget,37675,true);
                     return;
+                case 40802:                                 // Mingo's Fortune Generator (Mingo's Fortune Giblets)
+                {
+                    // selecting one from Bloodstained Fortune item
+                    uint32 newitemid;
+                    switch(urand(1,20))
+                    {
+                        case 1:  newitemid = 32688; break;
+                        case 2:  newitemid = 32689; break;
+                        case 3:  newitemid = 32690; break;
+                        case 4:  newitemid = 32691; break;
+                        case 5:  newitemid = 32692; break;
+                        case 6:  newitemid = 32693; break;
+                        case 7:  newitemid = 32700; break;
+                        case 8:  newitemid = 32701; break;
+                        case 9:  newitemid = 32702; break;
+                        case 10: newitemid = 32703; break;
+                        case 11: newitemid = 32704; break;
+                        case 12: newitemid = 32705; break;
+                        case 13: newitemid = 32706; break;
+                        case 14: newitemid = 32707; break;
+                        case 15: newitemid = 32708; break;
+                        case 16: newitemid = 32709; break;
+                        case 17: newitemid = 32710; break;
+                        case 18: newitemid = 32711; break;
+                        case 19: newitemid = 32712; break;
+                        case 20: newitemid = 32713; break;
+                        default:
+                            return;
+                    }
+
+                    DoCreateItem(i,newitemid);
+                    return;
+                }
+                // Demon Broiled Surprise
+                /* FIX ME: Required for correct work implementing implicit target 7 (in pair (22,7))
+                case 43723:
+                {
+                    if (m_caster->GetTypeId() != TYPEID_PLAYER)
+                        return;
+
+                    ((Player*)m_caster)->CastSpell(unitTarget, 43753, true);
+                    return;
+                }
+                */
                 case 44875:                                 // Complete Raptor Capture
                 {
                     if(!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT)
@@ -1139,6 +1183,7 @@ void Spell::EffectDummy(uint32 i)
                 }
             }
             break;
+        }
         case SPELLFAMILY_MAGE:
             switch(m_spellInfo->Id )
             {
