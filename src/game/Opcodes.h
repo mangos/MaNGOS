e5b2d07b2315c32221fafcd34d01d45aab350575
VladimirMangos <vladimir@getmangos.com>
2008-12-20 03:47:29 +0300

    [6923] Fixed opcide table miscompilation at VC in x64 release mode.
    In 303 branch it showup as crash at most chat commands.

--- src/game/Opcodes.h
+++ src/game/Opcodes.h
@@ -25,6 +25,12 @@
 
 #include "Common.h"
 
+// Note: this include need for be sure have full definition of class WorldSession
+//       if this class definition not complite then VS for x64 release use different size for
+//       struct OpcodeHandler in this header and Opcode.cpp and get totally wrong data from
+//       table opcodeTable in source when Opcode.h included but WorldSession.h not included
+#include "WorldSession.h"
+
 /// List of Opcodes
 enum Opcodes
 {
@@ -1230,7 +1236,6 @@ enum SessionStatus
     STATUS_NEVER                                            ///< Opcode not accepted from client (deprecated or server side only)
 };
 
-class WorldSession;
 class WorldPacket;
 
 struct OpcodeHandler
