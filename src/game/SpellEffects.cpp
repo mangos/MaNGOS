96accd240e8fcbdc6adcfa04fc6f70fe71d5bdf8
VladimirMangos <vladimir@getmangos.com>
2008-12-28 20:02:00 +0300

    [6963] Fixed crash at glyph item use.
    Prevent double deleted cast item in spell effect.
    Thanks to GriffonHeart for testing.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -5237,8 +5237,6 @@ void Spell::EffectApplyGlyph(uint32 i)
 
             player->CastSpell(m_caster, gp->SpellId, true);
             player->SetGlyph(m_glyphIndex, glyph);
-            if(m_CastItem)
-                player->DestroyItemCount(m_CastItem->GetEntry(), 1, true);
         }
     }
 }
