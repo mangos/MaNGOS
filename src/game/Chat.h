9e3662f708e558e544ea98b7a7c9e91f982ac1dd
VladimirMangos <vladimir@getmangos.com>
2009-01-07 17:47:57 +0300

    [7046] Do more security level checks in commands using HasLowerSecurity. Make use .account set addon safe for players.

--- src/game/Chat.h
+++ src/game/Chat.h
@@ -77,7 +77,8 @@ class ChatHandler
 
         virtual bool isAvailable(ChatCommand const& cmd) const;
         virtual bool needReportToTarget(Player* chr) const;
-        bool HasLowerSecurity(Player* target, uint64 guid);
+        bool HasLowerSecurity(Player* target, uint64 guid, bool strong = false);
+        bool HasLowerSecurityAccount(WorldSession* target, uint32 account, bool strong = false);
 
         void SendGlobalSysMessage(const char *str);
 
