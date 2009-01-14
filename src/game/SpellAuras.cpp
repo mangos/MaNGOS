6d80b04b20ae2b424a5c0b3a474d543a5221377d
DiSlord <dislord@nomail.com>
2009-01-14 20:43:44 +0300

    Fix Night Elf race ability on stealth path by Kirix
    Signed-off-by: DiSlord <dislord@nomail.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3434,8 +3434,8 @@ void Aura::HandleModStealth(bool apply, bool Real)
                 m_target->SetVisibility(VISIBILITY_GROUP_STEALTH);
             }
 
-            // for RACE_NIGHTELF stealth
-            if(m_target->GetTypeId()==TYPEID_PLAYER && GetId()==20580)
+            // (RACE_NIGHTELF stealth apply also Elusiveness)
+            if(m_target->getRace() == RACE_NIGHTELF)
                 m_target->CastSpell(m_target, 21009, true, NULL, this);
         }
     }
@@ -3444,8 +3444,8 @@ void Aura::HandleModStealth(bool apply, bool Real)
         // only at real aura remove
         if(Real)
         {
-            // for RACE_NIGHTELF stealth
-            if(m_target->GetTypeId()==TYPEID_PLAYER && GetId()==20580)
+            // (RACE_NIGHTELF stealth remove Elusiveness)
+            if(m_target->getRace() == RACE_NIGHTELF)
                 m_target->RemoveAurasDueToSpell(21009);
 
             // if last SPELL_AURA_MOD_STEALTH and no GM invisibility
