00e3aa13eb4c5b25882061c9b33fc791f221b498
Gorlum13 <Gorlum13@mangos.ru>
2009-01-25 16:23:53 +0300

    [7174] Implement new option GM.Visible for set original GM visibility state at login.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -778,6 +778,7 @@ void World::LoadConfigSettings(bool reload)
     }
 
     m_configs[CONFIG_GM_LOGIN_STATE]       = sConfig.GetIntDefault("GM.LoginState", 2);
+    m_configs[CONFIG_GM_VISIBLE_STATE]     = sConfig.GetIntDefault("GM.Visible", 2);
     m_configs[CONFIG_GM_ACCEPT_TICKETS]    = sConfig.GetIntDefault("GM.AcceptTickets", 2);
     m_configs[CONFIG_GM_CHAT]              = sConfig.GetIntDefault("GM.Chat", 2);
     m_configs[CONFIG_GM_WISPERING_TO]      = sConfig.GetIntDefault("GM.WhisperingTo", 2);
