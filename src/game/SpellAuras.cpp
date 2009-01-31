a261afd5f08ef9773d0af83b4286b19d4172a4ae
KiriX <KiriX@mangos.ru>
2009-01-31 05:52:42 +0300

    [7206] Not set creep walking mode for recial stealth ability 58984.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3406,7 +3406,8 @@ void Aura::HandleModStealth(bool apply, bool Real)
         // only at real aura add
         if(Real)
         {
-            m_target->SetStandFlags(UNIT_STAND_FLAGS_CREEP);
+            if(GetId()!=SPELL_ID_SHADOWMELD)
+                m_target->SetStandFlags(UNIT_STAND_FLAGS_CREEP);
             if(m_target->GetTypeId()==TYPEID_PLAYER)
                 m_target->SetFlag(PLAYER_FIELD_BYTES2, 0x2000);
 
@@ -3426,7 +3427,8 @@ void Aura::HandleModStealth(bool apply, bool Real)
             // if last SPELL_AURA_MOD_STEALTH and no GM invisibility
             if(!m_target->HasAuraType(SPELL_AURA_MOD_STEALTH) && m_target->GetVisibility()!=VISIBILITY_OFF)
             {
-                m_target->RemoveStandFlags(UNIT_STAND_FLAGS_CREEP);
+                if(GetId()!=SPELL_ID_SHADOWMELD)
+                    m_target->RemoveStandFlags(UNIT_STAND_FLAGS_CREEP);
                 if(m_target->GetTypeId()==TYPEID_PLAYER)
                     m_target->RemoveFlag(PLAYER_FIELD_BYTES2, 0x2000);
 
