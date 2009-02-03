e646b4884cd5f684d24f86eb819d40154e4fabe2
VladimirMangos <vladimir@getmangos.com>
2009-02-04 02:06:42 +0300

    [7227] Implement prismatic sockets. Related code cleanups.
    Big thanks to TOM_RUS for help in reseach!

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -599,6 +599,7 @@ void Spell::FillTargetMap()
                     break;
                 case SPELL_EFFECT_ENCHANT_ITEM:
                 case SPELL_EFFECT_ENCHANT_ITEM_TEMPORARY:
+                case SPELL_EFFECT_ENCHANT_ITEM_PRISMATIC:
                 case SPELL_EFFECT_DISENCHANT:
                 case SPELL_EFFECT_PROSPECTING:
                 case SPELL_EFFECT_MILLING:
@@ -5066,6 +5067,7 @@ uint8 Spell::CheckItems()
                 break;
             }
             case SPELL_EFFECT_ENCHANT_ITEM:
+            case SPELL_EFFECT_ENCHANT_ITEM_PRISMATIC:
             {
                 Item* targetItem = m_targets.getItemTarget();
                 if(!targetItem)
