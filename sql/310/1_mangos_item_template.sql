c40f568fad25ae556361879e39f5558904088cd4
tomrus88 <tomrus88@gmail.com>
2009-02-27 21:16:33 +0300

    Added lost SQL update

---
+++ sql/310/1_mangos_item_template.sql
@@ -0,0 +1,10 @@
+alter table `item_template`
+    drop column `dmg_type3`,
+    drop column `dmg_max3`,
+    drop column `dmg_min3`,
+    drop column `dmg_type5`,
+    drop column `dmg_max5`,
+    drop column `dmg_min5`,
+    drop column `dmg_type4`,
+    drop column `dmg_max4`,
+    drop column `dmg_min4`;
