1dc5dba7d967671a8d86a219cc51a33b8a2fe250
VladimirMangos <vladimir@getmangos.com>
2009-03-07 10:50:23 +0300

    [7398] Now really really fix error message :/

--- src/game/LootMgr.cpp
+++ src/game/LootMgr.cpp
@@ -121,7 +121,7 @@ void LootStore::LoadLootTable()
 
             if(maxcount > std::numeric_limits<uint8>::max())
             {
-                sLog.outErrorDb("Table '%s' entry %d item %d: maxcount value (%u) to large. must be less %u - skipped", GetName(), entry, maxcount,std::numeric_limits<uint8>::max());
+                sLog.outErrorDb("Table '%s' entry %d item %d: maxcount value (%u) to large. must be less %u - skipped", GetName(), entry, item, maxcount,std::numeric_limits<uint8>::max());
                 continue;                                   // error already printed to log/console.
             }
 
