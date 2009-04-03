974b19d3b452706926c2e45595fd69513c072198
tomrus88 <tomrus88@gmail.com>
2009-04-03 11:21:41 +0400

    Updated to latest PTR build 9757

--- src/realmd/AuthCodes.h
+++ src/realmd/AuthCodes.h
@@ -66,8 +66,8 @@ enum LoginResult
 
 // we need to stick to 1 version or half of the stuff will work for someone
 // others will not and opposite
-// will only support WoW, WoW:TBC and WoW:WotLK 3.1.0 client build 9742...
+// will only support WoW, WoW:TBC and WoW:WotLK 3.1.0 client build 9757...
 
-#define EXPECTED_MANGOS_CLIENT_BUILD        {9742, 0}
+#define EXPECTED_MANGOS_CLIENT_BUILD        {9757, 0}
 
 #endif
