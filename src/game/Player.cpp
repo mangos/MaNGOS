6af2ad8743ee2bdf7476acc9a79f89cc245940ad
VladimirMangos <vladimir@getmangos.com>
2009-04-14 13:57:06 +0400

    [7669] Some lost commented code patch for currencies trading item move to empty slot.
    In fact it not change anything except GM item created trade:
    no currency items currently that not bind at picked up.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -9311,7 +9311,6 @@ uint8 Player::CanStoreItems( Item **pItems,int count) const
 
             if (b_found) continue;
 
-            /* until proper implementation
             if(pProto->BagFamily & BAG_FAMILY_MASK_CURRENCY_TOKENS)
             {
                 for(uint32 t = CURRENCYTOKEN_SLOT_START; t < CURRENCYTOKEN_SLOT_END; ++t)
@@ -9326,7 +9325,6 @@ uint8 Player::CanStoreItems( Item **pItems,int count) const
             }
 
             if (b_found) continue;
-            */
 
             for(int t = INVENTORY_SLOT_BAG_START; !b_found && t < INVENTORY_SLOT_BAG_END; t++)
             {
