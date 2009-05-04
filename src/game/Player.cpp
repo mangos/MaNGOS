6ff7c46b1f4506fa2c4fe5caeacb604ff4a0ccca
zhenya <zhenya@getmangos.ru>
2009-05-04 14:45:11 +0400

    [7758] Fixed gain reputation and xp reward between group mates in differents instances
    Signed-off-by: AlexDereka <dereka.alex@gmail.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18700,14 +18700,17 @@ void Player::RewardPlayerAndGroupAtEvent(uint32 creature_id, WorldObject* pRewar
 
 bool Player::IsAtGroupRewardDistance(WorldObject const* pRewardSource) const
 {
-    if(pRewardSource->GetDistance(this) <= sWorld.getConfig(CONFIG_GROUP_XP_DISTANCE))
+    if (GetInstanceId() != pRewardSource->GetInstanceId())
+        return false;
+
+    if (pRewardSource->GetDistance(this) <= sWorld.getConfig(CONFIG_GROUP_XP_DISTANCE))
         return true;
 
-    if(isAlive())
+    if (isAlive())
         return false;
 
     Corpse* corpse = GetCorpse();
-    if(!corpse)
+    if (!corpse)
         return false;
 
     return pRewardSource->GetDistance(corpse) <= sWorld.getConfig(CONFIG_GROUP_XP_DISTANCE);
