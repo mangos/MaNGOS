3b6f65a24a364d5fef64718c0b7fe310a4e4788c
AlexDereka <dereka.alex@gmail.com>
2009-05-04 16:17:45 +0400

    [7759] Fixed [7758] commit.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18700,10 +18700,7 @@ void Player::RewardPlayerAndGroupAtEvent(uint32 creature_id, WorldObject* pRewar
 
 bool Player::IsAtGroupRewardDistance(WorldObject const* pRewardSource) const
 {
-    if (GetInstanceId() != pRewardSource->GetInstanceId())
-        return false;
-
-    if (pRewardSource->GetDistance(this) <= sWorld.getConfig(CONFIG_GROUP_XP_DISTANCE))
+    if (pRewardSource->IsWithinDistInMap(this,sWorld.getConfig(CONFIG_GR OUP_XP_DISTANCE)))
         return true;
 
     if (isAlive())
@@ -18713,7 +18710,7 @@ bool Player::IsAtGroupRewardDistance(WorldObject const* pRewardSource) const
     if (!corpse)
         return false;
 
-    return pRewardSource->GetDistance(corpse) <= sWorld.getConfig(CONFIG_GROUP_XP_DISTANCE);
+    return pRewardSource->IsWithinDistInMap(corpse,sWorld.getConfig(CONFIG_GR OUP_XP_DISTANCE));
 }
 
 uint32 Player::GetBaseWeaponSkillValue (WeaponAttackType attType) const
