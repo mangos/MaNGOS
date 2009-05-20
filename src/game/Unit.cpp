5e363f96a0e235e4577f99ec3dc7a2b17a5aa223
VladimirMangos <vladimir@getmangos.com>
2009-05-21 03:41:15 +0400

    [7862] Fixed infinity loop at search spell related summoned gameobject in owner list.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4136,7 +4136,7 @@ DynamicObject * Unit::GetDynObject(uint32 spellId)
 
 GameObject* Unit::GetGameObject(uint32 spellId) const
 {
-    for (GameObjectList::const_iterator i = m_gameObj.begin(); i != m_gameObj.end();)
+    for (GameObjectList::const_iterator i = m_gameObj.begin(); i != m_gameObj.end(); ++i)
         if ((*i)->GetSpellId() == spellId)
             return *i;
 
