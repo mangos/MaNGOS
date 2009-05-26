cff3b201b643bd04359f8857d90ea6bb70ebbf2e
VladimirMangos <vladimir@getmangos.com>
2009-05-27 01:12:44 +0400

    [7898] Improve client error output at different mail send errors, cleanup code.

--- src/game/Player.h
+++ src/game/Player.h
@@ -1297,7 +1297,7 @@ class MANGOS_DLL_SPEC Player : public Unit
         void ClearComboPoints();
         void SendComboPoints();
 
-        void SendMailResult(uint32 mailId, uint32 mailAction, uint32 mailError, uint32 equipError = 0, uint32 item_guid = 0, uint32 item_count = 0);
+        void SendMailResult(uint32 mailId, MailResponseType mailAction, MailResponseResult mailError, uint32 equipError = 0, uint32 item_guid = 0, uint32 item_count = 0);
         void SendNewMail();
         void UpdateNextMailTimeAndUnreads();
         void AddNewMailDeliverTime(time_t deliver_time);
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "7897"
+ #define REVISION_NR "7898"
 #endif // __REVISION_NR_H__
