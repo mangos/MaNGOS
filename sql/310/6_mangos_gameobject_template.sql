7692209fbe6fa08cd7aa44d80ac45bb727108e4b
AlexDereka <dereka.alex@gmail.com>
2009-05-27 22:09:59 +0400

    Fixed gameobject_template.

--- sql/310/6_mangos_gameobject_template.sql
+++ sql/310/6_mangos_gameobject_template.sql
@@ -1,5 +1,5 @@
 alter table `gameobject_template`
-    add column `unk1` varchar(100) NOT NULL default '',
+    add column `unk1` varchar(100) NOT NULL default ''            after `castBarCaption`,
     add column `questItem1` int(11) UNSIGNED DEFAULT '0' NOT NULL after `size`,
     add column `questItem2` int(11) UNSIGNED DEFAULT '0' NOT NULL after `questItem1`,
     add column `questItem3` int(11) UNSIGNED DEFAULT '0' NOT NULL after `questItem2`,
