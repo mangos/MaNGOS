fb9f4acf4be59ccc5b6cd678119945a5db9a2a54
tomrus88 <tomrus88@gmail.com>
2009-06-14 13:59:07 +0400

    [8008] Fixed SMSG_RAID_INSTANCE_MESSAGE opcode.

--- src/game/Player.h
+++ src/game/Player.h
@@ -684,7 +684,8 @@ enum InstanceResetWarningType
     RAID_INSTANCE_WARNING_HOURS     = 1,                    // WARNING! %s is scheduled to reset in %d hour(s).
     RAID_INSTANCE_WARNING_MIN       = 2,                    // WARNING! %s is scheduled to reset in %d minute(s)!
     RAID_INSTANCE_WARNING_MIN_SOON  = 3,                    // WARNING! %s is scheduled to reset in %d minute(s). Please exit the zone or you will be returned to your bind location!
-    RAID_INSTANCE_WELCOME           = 4                     // Welcome to %s. This raid instance is scheduled to reset in %s.
+    RAID_INSTANCE_WELCOME           = 4,                    // Welcome to %s. This raid instance is scheduled to reset in %s.
+    RAID_INSTANCE_EXPIRED           = 5
 };
 
 struct MovementInfo
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "8007"
+ #define REVISION_NR "8008"
 #endif // __REVISION_NR_H__
