fd7acd2e9db52296804785fc9beb3ad6a1df85e0
NoFantasy <NoFantasy@getmangos.com>
2009-06-18 19:10:18 +0400

    [8040] Implement Creature::ForcedDespawn and ACTION_T_FORCE_DESPAWN for EventAI.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Creature.h
+++ src/game/Creature.h
@@ -629,6 +629,8 @@ class MANGOS_DLL_SPEC Creature : public Unit
         void RemoveCorpse();
         bool isDeadByDefault() const { return m_isDeadByDefault; };
 
+        void ForcedDespawn();
+
         time_t const& GetRespawnTime() const { return m_respawnTime; }
         time_t GetRespawnTimeEx() const;
         void SetRespawnTime(uint32 respawn) { m_respawnTime = respawn ? time(NULL) + respawn : 0; }
