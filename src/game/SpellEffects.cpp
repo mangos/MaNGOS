7e4e626c62a211c186825b10fb0a15f69e5fc2bc
hunuza <hunuza@gmail.com>
2009-06-20 23:24:48 +0200

    [8061] Fixed 12834 and ranks stacking behaviour.
    Signed-off-by: hunuza <hunuza@gmail.com>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -680,15 +680,24 @@ void Spell::EffectDummy(uint32 i)
 
                     switch (m_spellInfo->Id)
                     {
-                        case 12850: damage *= 0.2f; break;
-                        case 12162: damage *= 0.4f; break;
-                        case 12868: damage *= 0.6f; break;
+                        case 12162: damage *= 0.16f; break; // Rank 1
+                        case 12850: damage *= 0.32f; break; // Rank 2
+                        case 12868: damage *= 0.48f; break; // Rank 3
                         default:
                             sLog.outError("Spell::EffectDummy: Spell %u not handled in DW",m_spellInfo->Id);
                             return;
                     };
 
-                    int32 deepWoundsDotBasePoints0 = int32(damage / 4);
+                    // get remaining damage of old Deep Wound aura
+                    Aura* deepWound = unitTarget->GetAura(12721, 0);
+                    if(deepWound)
+                    {
+                        int32 remainingTicks = deepWound->GetAuraDuration() / deepWound->GetModifier()->periodictime;
+                        damage += remainingTicks * deepWound->GetModifier()->m_amount;
+                    }
+
+                    // 1 tick/sec * 6 sec = 6 ticks
+                    int32 deepWoundsDotBasePoints0 = int32(damage / 6);
                     m_caster->CastCustomSpell(unitTarget, 12721, &deepWoundsDotBasePoints0, NULL, NULL, true, NULL);
                     return;
                 }
