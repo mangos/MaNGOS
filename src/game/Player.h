dd129c9744146e853c490f2b01a395bde1c5ed57
Ambal <Ambal@getmangos.ru>
2009-06-22 13:40:54 +0400

    [8066] Fixed typo in Teleport call.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Player.h
+++ src/game/Player.h
@@ -885,7 +885,7 @@ class MANGOS_DLL_SPEC Player : public Unit
 
         bool TeleportTo(WorldLocation const &loc, uint32 options = 0)
         {
-            return TeleportTo(loc.mapid, loc.coord_x, loc.coord_y, loc.coord_z, options);
+            return TeleportTo(loc.mapid, loc.coord_x, loc.coord_y, loc.coord_z, loc.orientation, options);
         }
 
         void SetSummonPoint(uint32 mapid, float x, float y, float z)
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "8065"
+ #define REVISION_NR "8066"
 #endif // __REVISION_NR_H__
