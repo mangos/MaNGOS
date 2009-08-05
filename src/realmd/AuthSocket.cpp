199d42621e884505c0a3c610804ba9584c38e1a0
VladimirMangos <vladimir@getmangos.com>
2009-08-05 20:21:25 +0400

    [8318] Deleted as expected runnable objects at related Thread deleting for avoid memory leaks.

--- src/realmd/AuthSocket.cpp
+++ src/realmd/AuthSocket.cpp
@@ -926,7 +926,7 @@ bool AuthSocket::_HandleXferResume()
     ibuf.Read((char*)&start,sizeof(start));
     fseek(pPatch, start, 0);
 
-    ACE_Based::Thread u(*new PatcherRunnable(this));
+    ACE_Based::Thread u(new PatcherRunnable(this));
     return true;
 }
 
@@ -959,7 +959,7 @@ bool AuthSocket::_HandleXferAccept()
     ibuf.Remove(1);                                         // clear input buffer
     fseek(pPatch, 0, 0);
 
-    ACE_Based::Thread u(*new PatcherRunnable(this));
+    ACE_Based::Thread u(new PatcherRunnable(this));
     return true;
 }
 
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "8317"
+ #define REVISION_NR "8318"
 #endif // __REVISION_NR_H__
