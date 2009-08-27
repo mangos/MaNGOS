35a51b1cff74271ef829b7ca5e173e2352f769fa
VladimirMangos <vladimir@getmangos.com>
2009-08-27 16:09:47 +0400

    [8429] Not allow stacking SPELL_AURA_PERIODIC_ENERGIZE from diff casters.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -3437,17 +3437,17 @@ bool Unit::AddAura(Aura *Aur)
                 switch(aurName)
                 {
                     // DoT/HoT/etc
-                    case SPELL_AURA_PERIODIC_DAMAGE:    // allow stack
+                    case SPELL_AURA_PERIODIC_DAMAGE:        // allow stack
                     case SPELL_AURA_PERIODIC_DAMAGE_PERCENT:
                     case SPELL_AURA_PERIODIC_LEECH:
                     case SPELL_AURA_PERIODIC_HEAL:
                     case SPELL_AURA_OBS_MOD_HEALTH:
                     case SPELL_AURA_PERIODIC_MANA_LEECH:
-                    case SPELL_AURA_PERIODIC_ENERGIZE:
                     case SPELL_AURA_OBS_MOD_MANA:
                     case SPELL_AURA_POWER_BURN_MANA:
                         break;
-                    default:                            // not allow
+                    case SPELL_AURA_PERIODIC_ENERGIZE:      // all or self or clear non-stackable
+                    default:                                // not allow
                         // can be only single (this check done at _each_ aura add
                         RemoveAura(i2,AURA_REMOVE_BY_STACK);
                         stop = true;
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "8428"
+ #define REVISION_NR "8429"
 #endif // __REVISION_NR_H__
