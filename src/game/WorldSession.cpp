3d0508c4bc9df52472504c3e77cdd41a2ee9e3f3
tomrus88 <tomrus88@gmail.com>
2009-08-29 12:37:33 +0400

    Compile/merge fix.

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -649,10 +649,10 @@ void WorldSession::SendAccountDataTimes(uint32 mask)
     WorldPacket data( SMSG_ACCOUNT_DATA_TIMES, 4+1+4+8*4 ); // changed in WotLK
     data << uint32(time(NULL));                             // unix time of something
     data << uint8(1);
-    data << uint32(mask);
+    data << uint32(mask);                                   // type mask
     for(int i = 0; i < NUM_ACCOUNT_DATA_TYPES; ++i)
         if(mask & (1 << i))
-            data << uint32(GetAccountData(i)->Time);        // also unix time
+            data << uint32(GetAccountData(AccountDataType(i))->Time);// also unix time
     SendPacket(&data);
 }
 
