1c3af1e0dea75c5a40f032c38569cb8b6f2e22dc
ksergik <ksergik@getmangos.ru>
2009-09-01 16:33:28 +0400

    [8448] Fixed loaded spell target position records in log.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -708,8 +708,6 @@ void SpellMgr::LoadSpellTargetPositions()
 
         bar.step();
 
-        ++count;
-
         uint32 Spell_ID = fields[0].GetUInt32();
 
         SpellTargetPosition st;
@@ -756,6 +754,7 @@ void SpellMgr::LoadSpellTargetPositions()
         }
 
         mSpellTargetPositions[Spell_ID] = st;
+        ++count;
 
     } while( result->NextRow() );
 
