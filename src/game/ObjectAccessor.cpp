cd575c71ce6c181149c1b2ae51c7d897f5d6e8f3
SilverIce <SilverIce@getmangos.com>
2009-09-11 22:23:52 +0400

    [8489] Fixed player visibility update in case view point different from player itself.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/ObjectAccessor.cpp
+++ src/game/ObjectAccessor.cpp
@@ -451,12 +451,23 @@ ObjectAccessor::UpdateObjectVisibility(WorldObject *obj)
 
 void ObjectAccessor::UpdateVisibilityForPlayer( Player* player )
 {
-    CellPair p = MaNGOS::ComputeCellPair(player->GetPositionX(), player->GetPositionY());
-    Cell cell(p);
+    WorldObject const* viewPoint = player->GetViewPoint();
     Map* m = player->GetMap();
 
+    CellPair p(MaNGOS::ComputeCellPair(player->GetPositionX(), player->GetPositionY()));
+    Cell cell(p);
+
     m->UpdatePlayerVisibility(player, cell, p);
-    m->UpdateObjectsVisibilityFor(player, cell, p);
+
+    if (player!=viewPoint)
+    {
+        CellPair pView(MaNGOS::ComputeCellPair(viewPoint->GetPositionX(), viewPoint->GetPositionY()));
+        Cell cellView(pView);
+
+        m->UpdateObjectsVisibilityFor(player, cellView, pView);
+    }
+    else
+        m->UpdateObjectsVisibilityFor(player, cell, p);
 }
 
 /// Define the static member of HashMapHolder
