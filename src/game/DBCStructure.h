0ed6b235740d9f6706f53434fcefebae7f2dc2d8
tomrus88 <tomrus88@gmail.com>
2009-09-14 16:32:34 +0400

    Added comment.

--- src/game/DBCStructure.h
+++ src/game/DBCStructure.h
@@ -645,7 +645,7 @@ struct ChrRacesEntry
     uint32      TeamID;                                     // 7 (7-Alliance 1-Horde)
                                                             // 8-11 unused
     uint32      CinematicSequence;                          // 12 id from CinematicSequences.dbc
-    //uint32    unk_322;                                    // 13
+    //uint32    unk_322;                                    // 13 faction (0 alliance, 1 horde, 2 not available?)
     char*       name[16];                                   // 14-29 used for DBC language detection/selection
                                                             // 30 string flags, unused
     //char*       nameFemale[16];                           // 31-46, if different from base (male) case
