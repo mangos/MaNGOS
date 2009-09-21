7312ecc67f51d173aa66ccdfa41808f551629274
Lutik <Lutik@getmangos.ru>
2009-09-21 23:32:19 +0400

    [8523] Add another versions of spell 44055 support.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4996,7 +4996,8 @@ void Aura::HandleAuraModIncreaseHealth(bool apply, bool Real)
         case 12976:                                         // Warrior Last Stand triggered spell
         case 28726:                                         // Nightmare Seed ( Nightmare Seed )
         case 34511:                                         // Valor (Bulwark of Kings, Bulwark of the Ancient Kings)
-        case 44055:                                         // Tremendous Fortitude (Battlemaster's Alacrity)
+        // FIXME: add case 67596: in 3.2.x 
+        case 44055: case 55915: case 55917:                 // Tremendous Fortitude (Battlemaster's Alacrity)
         case 50322:                                         // Survival Instincts
         case 54443:                                         // Demonic Empowerment (Voidwalker)
         {
