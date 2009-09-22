4ae8c0483523be75b7fd2a4829223dfaea0c239f
tomrus88 <tomrus88@gmail.com>
2009-09-22 18:15:18 +0400

    Updated to 3.2.2 live client build 10482. Fixed corpse query packet (it was broken since 3.1.x).

--- src/game/QueryHandler.cpp
+++ src/game/QueryHandler.cpp
@@ -324,6 +324,7 @@ void WorldSession::HandleCorpseQueryOpcode(WorldPacket & /*recv_data*/)
     data << float(y);
     data << float(z);
     data << int32(corpsemapid);
+    data << uint32(0);                                      // unknown
     SendPacket(&data);
 }
 
--- src/game/UpdateFields.h
+++ src/game/UpdateFields.h
@@ -19,7 +19,7 @@
 #ifndef _UPDATEFIELDS_AUTO_H
 #define _UPDATEFIELDS_AUTO_H
 
-// Auto generated for version 0, 2, 2, 10433
+// Auto generated for version 3, 2, 2, 10482
 
 enum EObjectFields
 {
