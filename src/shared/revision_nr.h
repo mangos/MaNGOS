4b4050fe01cfdb4b0a0754ed60514713370e26c1
Ambal <pogrebniak@gala.net>
2009-09-25 10:29:23 +0300

    [8528] Implementing implicit target 49, 50. Thanks to qsa.
    Signed-off-by: Ambal <pogrebniak@gala.net>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,8 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
+<<<<<<< HEAD
  #define REVISION_NR "8521"
+=======
+ #define REVISION_NR "8528"
+>>>>>>> [8528] Implementing implicit target 49, 50. Thanks to qsa.
 #endif // __REVISION_NR_H__
