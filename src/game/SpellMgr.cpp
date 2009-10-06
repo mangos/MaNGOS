2ecac00915273a77dca99d232e89c32de571bc92
Insider <insider42rus@yandex.ru>
2009-10-06 21:52:31 +0800

    [patch 31] Fix: prevent replacing spell 588 and ranks buff by 26573 and ranks

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -1584,6 +1584,11 @@ bool SpellMgr::IsNoStackSpellDueToSpell(uint32 spellId_1, uint32 spellId_2) cons
                     return false;
             }
 
+            // Inner Fire and Consecration
+            if(spellInfo_2->SpellFamilyName == SPELLFAMILY_PRIEST)
+                if(spellInfo_1->SpellIconID == 51 && spellInfo_2->SpellIconID == 51)
+                return false;
+
             // Combustion and Fire Protection Aura (multi-family check)
             if( spellInfo_2->Id == 11129 && spellInfo_1->SpellIconID == 33 && spellInfo_1->SpellVisual[0] == 321 )
                 return false;
