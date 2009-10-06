e1400b365d0ee6ee1e9777aee78a081fd3aa2ce0
balrok <balrok@getmangos.com>
2009-10-07 00:30:15 +0200

    [8595] fixed incompatible mysql
    in 8589_06 i got an error:
    ERROR 1170 (42000) at line 2: BLOB/TEXT column 'type' used in key specification without a key length
    
    also added delete-queries for 8589_05

--- sql/updates/8589_06_characters_bugreport.sql
+++ sql/updates/8589_06_characters_bugreport.sql
@@ -1,5 +1,4 @@
 ALTER TABLE character_db_version CHANGE COLUMN required_8589_04_characters_groups required_8589_06_characters_bugreport bit;
 
-alter table `characters`.`bugreport`
-    change `type` `type` longtext NOT NULL,
-    change `content` `content` longtext NOT NULL;
+ALTER TABLE `bugreport` CHANGE `content` `content` LONGTEXT;
+ALTER TABLE `bugreport` CHANGE `type` `type` LONGTEXT;
