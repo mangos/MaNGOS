06bace22261b1325b872369987a4e912be4de42f
Insider <insider42rus@yandex.ru>
2009-10-07 00:16:31 +0800

    [patch 75] Fixed talents 16086, 51523 and ranks

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6186,6 +6186,15 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                     triggered_spell_id = 64930;            // Electrified
                     break;
                 }
+                // Improved Fire Nova (Rank 2)
+                case 16544:
+                    triggered_spell_id = 51880;
+                    break;
+                // Earthen Power (Rank 1,2)
+                case 51523:
+                case 51524:
+                    triggered_spell_id = 63532;
+                    break;
             }
             // Storm, Earth and Fire
             if (dummySpell->SpellIconID == 3063)
