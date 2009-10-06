596d04a415b1a49ebe12c4bbb6c35e2bda0866c1
Insider <insider42rus@yandex.ru>
2009-10-06 21:40:08 +0800

    [patch 28] Fixed proc target for spell 38164

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7000,6 +7000,13 @@ bool Unit::HandleProcTriggerSpell(Unit *pVictim, uint32 damage, Aura* triggeredB
                 return false;
             break;
         }
+        // Unyielding Knights
+        case 38164:
+        {
+            if (pVictim->GetEntry()!=19457)
+                return false;
+            break;
+        }
         // Bonus Healing (Crystal Spire of Karabor mace)
         case 40971:
         {
