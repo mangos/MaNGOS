76a43bbc7f39193d3e78fd243d5902db7a9274b8
Insider <insider42rus@yandex.ru>
2009-10-06 22:59:26 +0800

    [patch 54] Added temporary fix for DK start zone. Now it is sanctuary zone.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -6298,7 +6298,7 @@ void Player::UpdateZone(uint32 newZone, uint32 newArea)
             pvpInfo.endTimer = time(0);                     // start toggle-off
     }
 
-    if(zone->flags & AREA_FLAG_SANCTUARY)                   // in sanctuary
+    if(zone->flags & AREA_FLAG_SANCTUARY || GetZoneId() == 4298)                   // in sanctuary
     {
         SetByteFlag(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_SANCTUARY);
         if(sWorld.IsFFAPvPRealm())
