39a00957d7e49aeb60ebb879a414e8ed15f23e11
Insider <insider42rus@yandex.ru>
2009-10-06 21:09:53 +0800

    [patch 12] Fix: not allow to set cooldown for triggered spells at login. This fixes bug with setting cooldown for spell 20608 at login

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2811,7 +2811,7 @@ void Spell::SendSpellCooldown()
     }
 
     // have infinity cooldown but set at aura apply
-    if(m_spellInfo->Attributes & SPELL_ATTR_DISABLED_WHILE_ACTIVE)
+    if(m_spellInfo->Attributes & SPELL_ATTR_DISABLED_WHILE_ACTIVE || m_IsTriggeredSpell)
         return;
 
     _player->AddSpellAndCategoryCooldowns(m_spellInfo,m_CastItem ? m_CastItem->GetEntry() : 0, this);
