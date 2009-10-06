e73d08c0a51301de7b6b325120c5474cadab4082
Insider <insider42rus@yandex.ru>
2009-10-06 21:18:49 +0800

    [patch 14] Fix: not allow to apply stun effects at taxi flight. This fixes bug with instant teleport to end point of taxi flight when using some sort of punishment from Lexics Cutter

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3645,6 +3645,9 @@ void Aura::HandleAuraModDisarm(bool apply, bool Real)
 
 void Aura::HandleAuraModStun(bool apply, bool Real)
 {
+    if(m_target->isInFlight())
+        return;
+
     if(!Real)
         return;
 
