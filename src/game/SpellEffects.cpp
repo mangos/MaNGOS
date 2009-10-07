ed65e27b7663ce63d156ddd54c52841292d6b824
Insider <insider42rus@yandex.ru>
2009-10-07 19:38:39 +0800

    [patch 99] Fixed spell 54711

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3314,6 +3314,7 @@ void Spell::EffectSummonType(uint32 i)
         case SUMMON_TYPE_WILD2:
         case SUMMON_TYPE_QUEST_WILD:
         case SUMMON_TYPE_CREATURE:
+        case SUMMON_TYPE_SCRAPBOT:
             EffectSummonWild(i);
             break;
         case SUMMON_TYPE_DEMON:
