e411d3f564701e158dbf1c51747d83492b218701
balrok <balrok@getmangos.com>
2009-10-18 04:25:19 -0400

    [8804] changed some unhandled opcode-errors to debug
    with this i also introduced a new status for opcodes
    STATUS_UNHANDLED
    
    not sure if i realy found all unhanddled opcodes
    those are all based on empircal analysis of our serverlog

--- src/game/Opcodes.h
+++ src/game/Opcodes.h
@@ -1317,7 +1317,8 @@ enum SessionStatus
     STATUS_LOGGEDIN,                                        ///< Player in game (_player!=NULL, m_GUID == _player->GetGUID(), inWorld())
     STATUS_TRANSFER,                                        ///< Player transferring to another map (_player!=NULL, m_GUID == _player->GetGUID(), !inWorld())
     STATUS_LOGGEDIN_OR_RECENTLY_LOGGOUT,                    ///< _player!= NULL or _player==NULL && m_playerRecentlyLogout, m_GUID store last _player guid)
-    STATUS_NEVER                                            ///< Opcode not accepted from client (deprecated or server side only)
+    STATUS_NEVER,                                           ///< Opcode not accepted from client (deprecated or server side only)
+    STATUS_UNHANDLED                                        ///< We don' handle this opcode yet
 };
 
 class WorldPacket;
