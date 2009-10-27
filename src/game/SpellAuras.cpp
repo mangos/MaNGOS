d4c3c5c6252977bfa1c436ee2e31f3c25f544c19
VladimirMangos <vladimir@getmangos.com>
2009-10-27 13:32:25 +0300

    [8745] Undo part of [8617] for restore risilience apply to DoT damage

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6382,6 +6382,11 @@ void Aura::PeriodicTick()
             if (isCrit)
                 cleanDamage.hitOutCome = MELEE_HIT_CRIT;
 
+            // Reduce dot damage from resilience for players.
+            // FIXME: need use SpellDamageBonus instead?
+            if (m_target->GetTypeId() == TYPEID_PLAYER)
+                pdamage-=((Player*)m_target)->GetSpellDamageReduction(pdamage);
+
             pCaster->CalcAbsorbResist(m_target, GetSpellSchoolMask(GetSpellProto()), DOT, pdamage, &absorb, &resist);
 
             sLog.outDetail("PeriodicTick: %u (TypeId: %u) attacked %u (TypeId: %u) for %u dmg inflicted by %u abs is %u",
