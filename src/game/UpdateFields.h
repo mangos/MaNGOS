eee42e2f5821775412d70f8ad0c5fb19d3f71126
tomrus88 <tomrus88@gmail.com>
2009-10-28 05:29:49 +0300

    Updated to latest PTR build 10712.

--- src/game/UpdateFields.h
+++ src/game/UpdateFields.h
@@ -19,7 +19,7 @@
 #ifndef _UPDATEFIELDS_AUTO_H
 #define _UPDATEFIELDS_AUTO_H
 
-// Auto generated for version 0, 3, 0, 10676
+// Auto generated for version 0, 3, 0, 10712
 
 enum EObjectFields
 {
--- src/realmd/AuthCodes.h
+++ src/realmd/AuthCodes.h
@@ -66,9 +66,9 @@ enum LoginResult
 
 // we need to stick to 1 version or half of the stuff will work for someone
 // others will not and opposite
-// will only support WoW, WoW:TBC and WoW:WotLK 3.3.0 client build 10676...
+// will only support WoW, WoW:TBC and WoW:WotLK 3.3.0 client build 10712...
 
-#define EXPECTED_MANGOS_CLIENT_BUILD        {10676, 0}
+#define EXPECTED_MANGOS_CLIENT_BUILD        {10712, 0}
 
 // At update excepted builds please update if need define DEFAULT_MAX_LEVEL
 // in DBCEnum.h to default max player level expected by build
