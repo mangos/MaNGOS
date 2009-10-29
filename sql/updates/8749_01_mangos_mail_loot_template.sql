748dcef4621278062fb3c6d2e2ae2f20d6d36688
VladimirMangos <vladimir@getmangos.com>
2009-10-29 09:25:22 +0300

    [8750] Fix sql update for [8749]. Use this version.

--- sql/updates/8749_01_mangos_mail_loot_template.sql
+++ sql/updates/8749_01_mangos_mail_loot_template.sql
@@ -3,4 +3,4 @@ ALTER TABLE db_version CHANGE COLUMN required_8731_01_mangos_creature_template r
 RENAME TABLE quest_mail_loot_template TO mail_loot_template;
 
 UPDATE mail_loot_template, quest_template
-  SET entry = quest_template.RewMailTemplateId WHERE mail_loot_template.entry = quest_template.entry;
+  SET mail_loot_template.entry = quest_template.RewMailTemplateId WHERE mail_loot_template.entry = quest_template.entry;
