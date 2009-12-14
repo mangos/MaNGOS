b74793fbec6c43f13e4d4858822f436a7e703044
NoFantasy <nofantasy@nf.no>
2009-12-14 10:45:29 +0100

    [8990] Correctly return DEFAULT_GOSSIP_MESSAGE and never 0 as gossipTextId
    Fixes problem with summoned and gossip and where no textId is defined in database
    
    Signed-off-by: NoFantasy <nofantasy@nf.no>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -12498,7 +12498,7 @@ void Player::OnGossipSelect(WorldObject* pSource, uint32 gossipListId, uint32 me
 uint32 Player::GetGossipTextId(WorldObject *pSource)
 {
     if (!pSource || pSource->GetTypeId() != TYPEID_UNIT || !((Creature*)pSource)->GetDBTableGUIDLow())
-        return 0;
+        return DEFAULT_GOSSIP_MESSAGE;
 
     if (uint32 pos = sObjectMgr.GetNpcGossip(((Creature*)pSource)->GetDBTableGUIDLow()))
         return pos;
