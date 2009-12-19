1e66682187f53857c2a2aac735b30d0db2e211c7
laise <laise@getmangos.com>
2009-12-19 06:24:58 +0300

    [9025] Implement talent 63373 and ranks.
    Idea with AddTriggeredSpell use suggested by darkstalker.
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6594,7 +6594,25 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                     }
                 }
                 return false;
-                break;
+            }
+            // Frozen Power
+            if (dummySpell->SpellIconID == 3780)
+            {
+                Unit *caster = triggeredByAura->GetCaster();
+
+                if (!procSpell || !caster)
+                    return false;
+
+                float distance = caster->GetDistance(pVictim);
+                int32 chance = triggerAmount;
+
+                if (distance < 15.0f || !roll_chance_i(chance))
+                    return false;
+
+                // make triggered cast apply after current damage spell processing for prevent remove by it
+                if(Spell* spell = GetCurrentSpell(CURRENT_GENERIC_SPELL))
+                    spell->AddTriggeredSpell(63685);
+                return true;
             }
             break;
         }
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_8874_01_characters_character_skills"
- #define REVISION_DB_MANGOS "required_9019_01_mangos_spell_threat"
+ #define REVISION_DB_MANGOS "required_9025_01_mangos_spell_proc_event"
  #define REVISION_DB_REALMD "required_9010_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__
