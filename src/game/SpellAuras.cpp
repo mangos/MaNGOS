c6d658502c95940c0021b6691aa391af36de722e
tehmarto <tehmarto@getmangos.com>
2009-12-24 04:48:16 +0300

    [9055] Fixed charge amount for spell 64823 to expected 1
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1391,6 +1391,7 @@ void Aura::HandleAddModifier(bool apply, bool Real)
             case 51124:                                     // Killing Machine
             case 54741:                                     // Firestarter
             case 57761:                                     // Fireball!
+            case 64823:                                     // Elune's Wrath (Balance druid t8 set
                 SetAuraCharges(1);
                 break;
         }
