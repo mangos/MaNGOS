8f5fd05fdd7c2545df66b7a03d99551ce0943cd7
MrLama <MrLama@getmangos.com>
2009-12-25 22:11:04 +0300

    [9068] Include spell 35395 in trigger list for talent 53380 and ranks.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    Also remove wrong masks from talent 53486 and ranks.
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

---
+++ sql/updates/9068_01_mangos_spell_proc_event.sql
@@ -0,0 +1,10 @@
+ALTER TABLE db_version CHANGE COLUMN required_9064_01_mangos_spell_proc_event required_9068_01_mangos_spell_proc_event bit;
+
+DELETE FROM spell_proc_event WHERE entry IN (53380,53381,53382);
+INSERT INTO spell_proc_event VALUES
+(53380, 0x00000000, 10, 0x00800000, 0x00028000, 0x00000000, 0x00000000, 0x00000002, 0.000000, 0.000000,  0);
+
+
+DELETE FROM spell_proc_event WHERE entry IN (53486,53488);
+INSERT INTO spell_proc_event VALUES
+(53486, 0x00000000,  0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000002, 0.000000, 0.000000,  0);
