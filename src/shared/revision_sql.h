5db860d9617a61e8e52fa729b820bf0811ec9e62
KAPATEJIb <KAPATEJIb@getmangos.com>
2010-01-12 06:32:52 +0300

    [9155] Update spell_proc_event for spell 20375, 21084, 31801.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_9136_07_characters_characters"
- #define REVISION_DB_MANGOS "required_9153_01_mangos_spell_bonus_data"
+ #define REVISION_DB_MANGOS "required_9155_01_mangos_spell_proc_event"
  #define REVISION_DB_REALMD "required_9010_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__
