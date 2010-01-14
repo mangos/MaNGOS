e043d12346f41faa1f324651db4bce54b414fc80
Rockette <Rockette@udw.altervista.org>
2010-01-14 13:05:50 +0300

    [9174] Target limit for spell 25991.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1348,6 +1348,9 @@ void Spell::SetTargetMap(uint32 effIndex, uint32 targetMode, UnitList& targetUni
                 case 54835:                                 // Curse of the Plaguebringer (h)
                     unMaxTargets = 10;
                     break;
+                case 25991:                                 // Poison Bolt Volley (Pincess Huhuran)
+                    unMaxTargets = 15;
+                    break;
             }
             break;
         }
