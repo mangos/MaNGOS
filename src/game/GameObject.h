a3ff8c54201b62f876d34822765371901effa5eb
NoFantasy <nofantasy@nf.no>
2010-01-17 02:11:51 +0100

    [9195] Update some comments for GO autoCloseTime to reflect changes in 9179
    Signed-off-by: NoFantasy <nofantasy@nf.no>

--- src/game/GameObject.h
+++ src/game/GameObject.h
@@ -53,7 +53,7 @@ struct GameObjectInfo
         {
             uint32 startOpen;                               //0 used client side to determine GO_ACTIVATED means open/closed
             uint32 lockId;                                  //1 -> Lock.dbc
-            uint32 autoCloseTime;                           //2 secs till autoclose = autoCloseTime / 0x10000
+            uint32 autoCloseTime;                           //2 secs till autoclose = autoCloseTime / IN_MILISECONDS (previous was 0x10000)
             uint32 noDamageImmune;                          //3 break opening whenever you recieve damage?
             uint32 openTextID;                              //4 can be used to replace castBarCaption?
             uint32 closeTextID;                             //5
@@ -64,7 +64,7 @@ struct GameObjectInfo
         {
             uint32 startOpen;                               //0
             uint32 lockId;                                  //1 -> Lock.dbc
-            uint32 autoCloseTime;                           //2 secs till autoclose = autoCloseTime / 0x10000
+            uint32 autoCloseTime;                           //2 secs till autoclose = autoCloseTime / IN_MILISECONDS (previous was 0x10000)
             uint32 linkedTrap;                              //3
             uint32 noDamageImmune;                          //4 isBattlegroundObject
             uint32 large;                                   //5
@@ -127,7 +127,7 @@ struct GameObjectInfo
             uint32 spellId;                                 //3
             uint32 charges;                                 //4 need respawn (if > 0)
             uint32 cooldown;                                //5 time in secs
-            uint32 autoCloseTime;                           //6
+            uint32 autoCloseTime;                           //6 secs till autoclose = autoCloseTime / IN_MILISECONDS (previous was 0x10000)
             uint32 startDelay;                              //7
             uint32 serverOnly;                              //8
             uint32 stealthed;                               //9
@@ -170,7 +170,7 @@ struct GameObjectInfo
             uint32 lockId;                                  //0 -> Lock.dbc
             uint32 questId;                                 //1
             uint32 eventId;                                 //2
-            uint32 autoCloseTime;                           //3
+            uint32 autoCloseTime;                           //3 secs till autoclose = autoCloseTime / IN_MILISECONDS (previous was 0x10000)
             uint32 customAnim;                              //4
             uint32 consumable;                              //5
             uint32 cooldown;                                //6
@@ -194,7 +194,7 @@ struct GameObjectInfo
         {
             uint32 pause;                                   //0
             uint32 startOpen;                               //1
-            uint32 autoCloseTime;                           //2 secs till autoclose = autoCloseTime / 0x10000
+            uint32 autoCloseTime;                           //2 secs till autoclose = autoCloseTime / IN_MILISECONDS (previous was 0x10000)
             uint32 pause1EventID;                           //3
             uint32 pause2EventID;                           //4
         } transport;
@@ -206,7 +206,7 @@ struct GameObjectInfo
             uint32 damageMin;                               //2
             uint32 damageMax;                               //3
             uint32 damageSchool;                            //4
-            uint32 autoCloseTime;                           //5 secs till autoclose = autoCloseTime / 0x10000
+            uint32 autoCloseTime;                           //5 secs till autoclose = autoCloseTime / IN_MILISECONDS (previous was 0x10000)
             uint32 openTextID;                              //6
             uint32 closeTextID;                             //7
         } areadamage;
