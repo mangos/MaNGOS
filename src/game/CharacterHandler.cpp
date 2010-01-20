c3e60ae88db5ef2d6dec34e163c42088b67d4e5c
Anubisss <Anubisss@getmangos.com>
2010-01-20 14:29:24 +0300

    [9224] Fixed memory leaks in WorldSession::HandleCharCreateOpcode
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/CharacterHandler.cpp
+++ src/game/CharacterHandler.cpp
@@ -359,6 +359,7 @@ void WorldSession::HandleCharCreateOpcode( WorldPacket & recv_data )
                     {
                         data << (uint8)CHAR_CREATE_UNIQUE_CLASS_LIMIT;
                         SendPacket( &data );
+                        delete result2;
                         return;
                     }
                 }
@@ -413,6 +414,7 @@ void WorldSession::HandleCharCreateOpcode( WorldPacket & recv_data )
                         {
                             data << (uint8)CHAR_CREATE_UNIQUE_CLASS_LIMIT;
                             SendPacket( &data );
+                            delete result2;
                             return;
                         }
                     }
