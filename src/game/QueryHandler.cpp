1dd846140078863301d72a1de8046a9703174437
VladimirMangos <vladimir@getmangos.com>
2010-01-20 07:28:16 +0300

    [9218] Fixed check in CMSG_QUEST_POI_QUERY proccessing for max quest amount.
    This fix work quest poi for filled quest list
    and fix unexpected spam about unreaded packet content.

--- src/game/QueryHandler.cpp
+++ src/game/QueryHandler.cpp
@@ -490,8 +490,11 @@ void WorldSession::HandleQuestPOIQuery(WorldPacket& recv_data)
     uint32 count;
     recv_data >> count;                                     // quest count, max=25
 
-    if(count >= MAX_QUEST_LOG_SIZE)
+    if(count > MAX_QUEST_LOG_SIZE)
+    {
+        recv_data.rpos(recv_data.wpos());                   // set to end to avoid warnings spam
         return;
+    }
 
     WorldPacket data(SMSG_QUEST_POI_QUERY_RESPONSE, 4+(4+4)*count);
     data << uint32(count);                                  // count
