ba90f4119bd188caea923871c7776657f520d031
Wowka321 <Wowka321@getmangos.com>
2010-01-24 19:03:37 +0300

    [9251] Restore work spell 31687 and implement work glyph 50045.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1402,6 +1402,14 @@ void Spell::EffectDummy(uint32 i)
                     }
                     return;
                 }
+                case 31687:                                 // Summon Water Elemental
+                {
+                    if (m_caster->HasAura(70937))           // Glyph of Eternal Water (permanent limited by known spells version)
+                        m_caster->CastSpell(m_caster, 70908, true);
+                    else                                    // temporary version
+                        m_caster->CastSpell(m_caster, 70907, true);
+                    return;
+                }
                 case 32826:                                 // Polymorph Cast Visual
                 {
                     if (unitTarget && unitTarget->GetTypeId() == TYPEID_UNIT)
