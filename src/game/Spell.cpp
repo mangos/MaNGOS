f869aa30ef2c163016fed707ac4f7ee5caab4605
Insider <insider42rus@yandex.ru>
2010-02-07 17:07:24 +0700

    [patch 30] Fix: not allow to apply buff from 2825 and 32182 if present 57723 or 57724 debuff at target

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -6012,6 +6012,11 @@ bool Spell::CheckTarget( Unit* target, uint32 eff )
             return false;
     }
 
+    // Check Sated & Exhaustion debuffs
+    if (((m_spellInfo->Id == 2825) && (target->HasAura(57724))) ||
+        ((m_spellInfo->Id == 32182) && (target->HasAura(57723))))
+        return false;
+
     // Check targets for LOS visibility (except spells without range limitations )
     switch(m_spellInfo->Effect[eff])
     {
