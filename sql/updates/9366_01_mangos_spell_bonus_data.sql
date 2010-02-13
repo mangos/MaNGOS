15d71deccb937d95aed26a85a8b832599c59a127
VladimirMangos <vladimir@getmangos.com>
2010-02-13 20:32:36 +0300

    [9367] Fixes for [9366] for restore server startup and other.
    * Add expected sql update order guards to sql updates
    * Update size of spell id field in spell_bonus_data.
    
    Note: You need apply new sql updates versions or just re-aplly
          one more time new sql update version if apply already old.

--- sql/updates/9366_01_mangos_spell_bonus_data.sql
+++ sql/updates/9366_01_mangos_spell_bonus_data.sql
@@ -1,3 +1,9 @@
+ALTER TABLE db_version CHANGE COLUMN required_9331_01_mangos_quest_template required_9366_01_mangos_spell_bonus_data bit;
+
+ALTER TABLE spell_bonus_data
+  CHANGE COLUMN entry entry mediumint(8) unsigned NOT NULL;
+
+
 DELETE FROM spell_bonus_data WHERE entry = 71824;
 INSERT INTO spell_bonus_data VALUES
 (71824,0,0,0,'Item - Shaman T9 Elemental 4P Bonus');
--- sql/updates/9366_02_mangos_spell_proc_event.sql
+++ sql/updates/9366_02_mangos_spell_proc_event.sql
@@ -1,3 +1,5 @@
+ALTER TABLE db_version CHANGE COLUMN required_9366_01_mangos_spell_bonus_data required_9366_02_mangos_spell_proc_event bit;
+
 DELETE FROM `spell_proc_event` WHERE `entry` = 67228;
 INSERT INTO `spell_proc_event` VALUES
 (67228, 0x00000004, 11, 0x00000000, 0x00001000, 0x00000000, 0x00000000, 0x00000000, 0.000000, 0.000000, 0); 
