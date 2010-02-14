4e82786a67e2ca917fb5b70e825510713b05f591
VladimirMangos <vladimir@getmangos.com>
2010-02-14 15:39:06 +0300

    [9376] Maybe last fix of fix of fix sequence. O.O

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "9375"
+ #define REVISION_NR "9376"
 #endif // __REVISION_NR_H__
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_9375_01_characters_character_glyphs"
  #define REVISION_DB_MANGOS "required_9366_02_mangos_spell_proc_event"
- #define REVISION_DB_REALMD "required_"
+ #define REVISION_DB_REALMD "required_required_9010_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__
