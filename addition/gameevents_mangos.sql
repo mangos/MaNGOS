3ac3be39e5043209f50f9d4a182e189f5ee51217
/dev/rsa <kris13@bk.ru>
2010-02-20 18:51:29 +0300

    Gameevents sql file added

---
+++ addition/gameevents_mangos.sql
@@ -0,0 +1,2 @@
+UPDATE `mangos`.`game_event` SET `occurence` = '1440' WHERE `game_event`.`entry` =16;
+UPDATE `mangos`.`game_event` SET `start_time` = '2009-12-06 16:15:00',`occurence` = '7200',`length` = '60' WHERE `game_event`.`entry` =44 LIMIT 1 ;
