14de1c4ee9773dd08a24b647a1459b5b8a335a0f
Insider <insider42rus@yandex.ru>
2010-03-03 14:48:07 +0700

    Reverted [patch 86]

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2486,8 +2486,6 @@ void Spell::prepare(SpellCastTargets const* targets, Aura* triggeredByAura)
     // add non-triggered (with cast time and without)
     if (!m_IsTriggeredSpell)
     {
-        m_caster->RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_CAST);
-
         // add to cast type slot
         m_caster->SetCurrentCastedSpell( this );
 
