98eff592225d420219992b32d996af5c3897c8d2
KAPATEJIb <KAPATEJIb@mangos.ru>
2010-03-06 18:52:23 +0300

    [9534] Attack Power bonus for spell 64382.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -401,6 +401,9 @@ void Spell::EffectSchoolDMG(SpellEffectIndex effect_idx)
                 // Heroic Throw ${$m1+$AP*.50}
                 else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x0000000100000000))
                     damage+= uint32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.5f);
+                // Shattering Throw ${$m1+$AP*.50}
+                else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x0040000000000000))
+                    damage+= uint32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.5f);
                 // Shockwave ${$m3/100*$AP}
                 else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x0000800000000000))
                 {
