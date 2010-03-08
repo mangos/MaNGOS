418e31b855a1c9707d64de76561d72301670a2ad
ApoC <apoc@nymfe.net>
2010-03-08 22:31:24 +0100

    [9555] Makes some arguments as references in some ByteBuffer operator methods.
    Signed-off-by: ApoC <apoc@nymfe.net>

--- src/shared/ByteBuffer.h
+++ src/shared/ByteBuffer.h
@@ -506,7 +506,7 @@ class ByteBuffer
 };
 
 template <typename T>
-inline ByteBuffer &operator<<(ByteBuffer &b, std::vector<T> v)
+inline ByteBuffer &operator<<(ByteBuffer &b, std::vector<T> const& v)
 {
     b << (uint32)v.size();
     for (typename std::vector<T>::iterator i = v.begin(); i != v.end(); ++i)
@@ -532,7 +532,7 @@ inline ByteBuffer &operator>>(ByteBuffer &b, std::vector<T> &v)
 }
 
 template <typename T>
-inline ByteBuffer &operator<<(ByteBuffer &b, std::list<T> v)
+inline ByteBuffer &operator<<(ByteBuffer &b, std::list<T> const& v)
 {
     b << (uint32)v.size();
     for (typename std::list<T>::iterator i = v.begin(); i != v.end(); ++i)
