92c213a259c0466773336889954a7f872e3f40c3
Insider <insider42rus@yandex.ru>
2010-03-10 21:02:09 +0700

    [patch 109] Fixed ppm chance for spell 12322 and ranks. Patch provided by me

---
+++ addition/109_mangos_spell_proc_event.sql
@@ -0,0 +1,5 @@
+update `spell_proc_event` set `ppmRate` = 3 where `entry` = 12322;
+update `spell_proc_event` set `ppmRate` = 6 where `entry` = 12999;
+update `spell_proc_event` set `ppmRate` = 9 where `entry` = 13000;
+update `spell_proc_event` set `ppmRate` = 12 where `entry` = 13001;
+update `spell_proc_event` set `ppmRate` = 15 where `entry` = 13002;
