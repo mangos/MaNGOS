952410af5650ec2ddef7a5af74cdb8d2183c663a
Insider <insider42rus@yandex.ru>
2010-03-29 20:06:46 +0700

    [patch 126] Fixed a bug when some spells make damage to friendly targets and self. Patch provided by laise

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -6435,8 +6435,5 @@ void Spell::FillRaidOrPartyHealthPriorityTargets(UnitList &targetUnitMap, Unit* 
 
 WorldObject* Spell::GetCastingObject() const
 {
-    if (m_originalCasterGUID.IsGameobject())
-        return m_caster->IsInWorld() ? m_caster->GetMap()->GetGameObject(m_originalCasterGUID) : NULL;
-    else
-        return m_caster;
+    return m_caster->IsInWorld() && !m_originalCasterGUID.IsEmpty() && m_caster->GetObjectGuid() != m_originalCasterGUID ? m_caster->GetMap()->GetWorldObject(m_originalCasterGUID) : m_caster;
 }
