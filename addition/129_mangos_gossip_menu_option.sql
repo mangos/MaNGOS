ccceeb8ed37a3b70426ea5716f9b976290d93987
/dev/rsa <kris13@bk.ru>
2010-04-03 16:41:21 +0400

    Correct Insider's SQL for dualspec

--- addition/129_mangos_gossip_menu_option.sql
+++ addition/129_mangos_gossip_menu_option.sql
@@ -1,2 +1,3 @@
+DELETE FROM `gossip_menu_option` WHERE `gossip_menu_option`.`option_id` IN (18,19);
 INSERT INTO `gossip_menu_option` VALUES 
-(0, 16, 0, 'Purchase a Dual Talent Specialization.', 18, 16, 0, 0, 0, 0, 10000000, 'Are you sure you wish to purchase a Dual Talent Specialization?', 0, 0, 0, 0, 0, 0, 0, 0, 0);
+(0, 51, 0, 'Purchase a Dual Talent Specialization.', 18, 16, 0, 0, 0, 0, 10000000, 'Are you sure you wish to purchase a Dual Talent Specialization?', 0, 0, 0, 0, 0, 0, 0, 0, 0);
