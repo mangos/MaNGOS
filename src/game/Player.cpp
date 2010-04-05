0730c34ac1949a4d3271a6ccc37d6bfa023e22bb
VladimirMangos <vladimir@getmangos.com>
2010-04-05 22:06:32 +0400

    [9676] Not counting gems at broken item for meta gem requirements.
    This is fit as cleint show meta gem state and fix cheating with gems at broken items.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18562,7 +18562,7 @@ bool Player::EnchantmentFitsRequirements(uint32 enchantmentcondition, int8 slot)
         if(i == slot)
             continue;
         Item *pItem2 = GetItemByPos( INVENTORY_SLOT_BAG_0, i );
-        if(pItem2 && pItem2->GetProto()->Socket[0].Color)
+        if(pItem2 && !pItem2->IsBroken() && pItem2->GetProto()->Socket[0].Color)
         {
             for(uint32 enchant_slot = SOCK_ENCHANTMENT_SLOT; enchant_slot < SOCK_ENCHANTMENT_SLOT+3; ++enchant_slot)
             {
