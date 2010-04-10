310c1aff79331645da8d15f42cd6a207ae65563c
Insider <insider42rus@yandex.ru>
2010-04-10 20:19:45 +0700

    [patch 29] Fix: doesn't allow to remove 26013 spell buff by 31224

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2528,7 +2528,9 @@ void Spell::EffectTriggerSpell(SpellEffectIndex effIndex)
                 if( // ignore positive and passive auras
                     !iter->second->IsPositive() && !iter->second->IsPassive() &&
                     // ignore physical auras
-                    (GetSpellSchoolMask(iter->second->GetSpellProto()) & SPELL_SCHOOL_MASK_NORMAL)==0 )
+                    (GetSpellSchoolMask(iter->second->GetSpellProto()) & SPELL_SCHOOL_MASK_NORMAL)==0 &&
+                    // ignore deserter
+                    iter->second->GetSpellProto()->Id != 26013 )
                 {
                     m_caster->RemoveAurasDueToSpell(iter->second->GetSpellProto()->Id);
                     iter = Auras.begin();
