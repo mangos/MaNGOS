827968c101d97b0518227cb1a9e1e8bb91a8ff0e
/dev/rsa <kris13@bk.ru>
2010-04-11 12:03:40 +0400

    SPELL_AURA_MOD_POSSESS patch (from psz)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3802,6 +3802,8 @@ void Aura::HandleModPossess(bool apply, bool Real)
             ((Player*)m_target)->SetClientControl(m_target, 0);
         }
 
+        m_target->SetUInt32Value(UNIT_NPC_EMOTESTATE, EMOTE_ONESHOT_NONE);
+
         if(CharmInfo *charmInfo = m_target->InitCharmInfo(m_target))
             charmInfo->InitPossessCreateSpells();
 
@@ -3835,10 +3837,17 @@ void Aura::HandleModPossess(bool apply, bool Real)
 
         if(m_target->GetTypeId() == TYPEID_UNIT)
         {
+            m_target->CombatStop();
+            m_target->getHostileRefManager().clearReferences();
+            m_target->getThreatManager().clearReferences();
+
             ((Creature*)m_target)->AIM_Initialize();
 
             if (((Creature*)m_target)->AI())
+            {
                 ((Creature*)m_target)->AI()->AttackedBy(caster);
+                ((Creature*)m_target)->AI()->AttackStart(caster);
+            }
         }
     }
 }
