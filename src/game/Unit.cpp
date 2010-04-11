3051b03c39bfd7241f292d909c9c26c1930cac9c
Insider <insider42rus@yandex.ru>
2010-04-11 21:58:16 +0700

    [patch 52] Fixed spell 55680

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5969,6 +5969,13 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                     triggered_spell_id = 56131;
                     break;
                 }
+                // Glyph of Prayer of Healing
+                case 55680:
+                {
+                    basepoints[0] = int32(damage * 20 / 100 / 2);   // divided in two ticks
+                    triggered_spell_id = 56161;
+                    break;
+                }
             }
             break;
         }
