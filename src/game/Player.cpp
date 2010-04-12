2017393c8b445b0f2b46b8cd4ca160f20e411168
Insider <insider42rus@yandex.ru>
2010-04-12 18:26:46 +0700

    [patch 72] Fixed crash with Player::BuildPlayerRepop

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -4249,7 +4249,8 @@ void Player::BuildPlayerRepop()
     if(GetCorpse())
     {
         sLog.outError("BuildPlayerRepop: player %s(%d) already has a corpse", GetName(), GetGUIDLow());
-        ASSERT(false);
+        sLog.outError("Removing player %s(%d) corpse from DB", GetName(), GetGUIDLow());
+        CharacterDatabase.PExecute("DELETE FROM corpse WHERE player = '%d'",GetGUIDLow());
     }
 
     // create a corpse and place it at the player's location
