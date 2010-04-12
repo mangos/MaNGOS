78171af54f4efe78f25fdf1f79a3a9bbf7d6aced
przemratajczak <przemratajczak@getmangos.com>
2010-04-12 11:51:16 +0300

    [9734] Removed outdated code for 33702,33697,20572
    Signed-off-by: Laise <fenrisse@gmail.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2569,9 +2569,6 @@ void Spell::cast(bool skipCheck)
         {
             if (m_spellInfo->Mechanic == MECHANIC_BANDAGE)  // Bandages
                 AddPrecastSpell(11196);                     // Recently Bandaged
-            else if(m_spellInfo->SpellIconID == 1662 && m_spellInfo->AttributesEx & 0x20)
-                                                            // Blood Fury (Racial)
-                AddPrecastSpell(23230);                     // Blood Fury - Healing Reduction
             else if(m_spellInfo->Id == 20594)               // Stoneskin
                 AddTriggeredSpell(65116);                   // Stoneskin - armor 10% for 8 sec
             break;
