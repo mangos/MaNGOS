fbd58aff658fdca5c944054e641f3085e7273665
MrLama <MrLama@getmangos.com>
2010-04-12 12:38:34 +0300

    [9735] Fix proc for 53709 and ranks
    Signed-off-by: Laise <fenrisse@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "9734"
+ #define REVISION_NR "9735"
 #endif // __REVISION_NR_H__
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_9702_01_characters_item"
- #define REVISION_DB_MANGOS "required_9728_01_mangos_gossip_menu_option"
+ #define REVISION_DB_MANGOS "required_9735_02_mangos_spell_chain"
  #define REVISION_DB_REALMD "required_9010_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__
