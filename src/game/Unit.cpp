958dd01486aac3e0db1ed86e880160952d96f260
Insider <insider42rus@yandex.ru>
2010-04-16 22:51:55 +0700

    [patch 112] Fixed invisibility for Arena Preparation. Patch provided by me

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -10913,6 +10913,10 @@ bool Unit::isVisibleForOrDetect(Unit const* u, WorldObject const* viewPoint, boo
         invisible = false;
     }
 
+    // Arena preparation hack
+    if(HasAura(SPELL_ARENA_PREPARATION))
+        invisible = true;
+
     // special cases for always overwrite invisibility/stealth
     if(invisible || m_Visibility == VISIBILITY_GROUP_STEALTH)
     {
