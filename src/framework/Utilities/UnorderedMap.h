2d959e3c128a4601a2a2899516dae672cc3add54
XTZGZoReX <xtzgzorex@gmail.com>
2010-04-22 11:49:36 +0200

    [9775] Cleanups in framework library.
    * Removed last bits of threading in grid code.
    * Removed some weird and unneeded declarations.
    * General code style fixes.
    * (Perhaps some things I forgot.)
    
    Thanks to Lynx3d for the usual GCC-stabbing...

--- src/framework/Utilities/UnorderedMap.h
+++ src/framework/Utilities/UnorderedMap.h
@@ -52,11 +52,14 @@ using std::hash_map;
 
 namespace __gnu_cxx
 {
-    template<> struct hash<unsigned long long>
+    template<>
+    struct hash<unsigned long long>
     {
         size_t operator()(const unsigned long long &__x) const { return (size_t)__x; }
     };
-    template<typename T> struct hash<T *>
+
+    template<typename T>
+    struct hash<T *>
     {
         size_t operator()(T * const &__x) const { return (size_t)__x; }
     };
