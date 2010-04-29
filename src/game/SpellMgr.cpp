8f58591f47086ef402fe3b0e84671e637cf33fc9
VladimirMangos <vladimir@getmangos.com>
2010-04-29 15:25:28 +0400

    [9805] Fixed lost flight mount/form for GM in old contentes.

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -2964,8 +2964,8 @@ SpellCastResult SpellMgr::GetSpellAllowedInLocationError(SpellEntry const *spell
             return SPELL_FAILED_INCORRECT_AREA;
     }
 
-    // continent limitation (virtual continent)
-    if (spellInfo->AttributesEx4 & SPELL_ATTR_EX4_CAST_ONLY_IN_OUTLAND)
+    // continent limitation (virtual continent), ignore for GM
+    if ((spellInfo->AttributesEx4 & SPELL_ATTR_EX4_CAST_ONLY_IN_OUTLAND) && !(player && player->isGameMaster()))
     {
         uint32 v_map = GetVirtualMapForMapAndZone(map_id, zone_id);
         MapEntry const* mapEntry = sMapStore.LookupEntry(v_map);
