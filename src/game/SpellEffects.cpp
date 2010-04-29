09de359c0acadb99d5fadb76eb77ac5dddf0f065
Insider <insider42rus@yandex.ru>
2010-04-29 20:36:05 +0700

    Reverted [patch 42]

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -520,9 +520,7 @@ void Spell::EffectSchoolDMG(SpellEffectIndex effect_idx)
                     {
                         // DoT not have applied spell bonuses in m_amount
                         int32 damagetick = m_caster->SpellDamageBonus(unitTarget, aura->GetSpellProto(), aura->GetModifier()->m_amount, DOT);
-                        // Save value of further damage
-                        m_currentBasePoints[1] = damagetick * 2 / 3;
-                        damage += damagetick * 3;
+                        damage += damagetick * 4;
 
                         // Glyph of Conflagrate
                         if (!m_caster->HasAura(56235))
