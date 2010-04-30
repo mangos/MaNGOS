c6a08ae7aec800cd8d46bc2c4666e7607a22929d
/dev/rsa <kris13@bk.ru>
2010-04-30 19:02:40 +0400

    Small change for merge

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -773,14 +773,7 @@ void Spell::EffectSchoolDMG(SpellEffectIndex effect_idx)
                 else if (m_spellInfo->Id == 54158)
                 {
                     // [1 + 0.25 * SPH + 0.16 * AP]
-<<<<<<< HEAD:src/game/SpellEffects.cpp
-                    float ap = m_caster->GetTotalAttackPowerValue(BASE_ATTACK);
-                    int32 holy = m_caster->SpellBaseDamageBonusDone(GetSpellSchoolMask(m_spellInfo)) +
-                                 unitTarget->SpellBaseDamageBonusTaken(GetSpellSchoolMask(m_spellInfo));
-                    damage += int32(ap * 0.16f) + int32(holy * 25 / 100);
-=======
                     damage += int32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.16f);
->>>>>>> 7a2ed7529cba1396f2f9ef60b8201181c3c9cef8:src/game/SpellEffects.cpp
                 }
                 break;
             }
