546a54f19e22db9a5a0dc63de6bc9ce5c23ed954
Insider <insider42rus@yandex.ru>
2010-05-03 14:35:53 +0700

    Added new version of [patch 109]

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2257,6 +2257,10 @@ void Aura::TriggerSpell()
                     caster->CastSpell(target, trigger_spell_id, true, NULL, this);
                 return;
             }
+            // Intense Cold
+            case 48094:
+                target->CastSpell(target, trigger_spell_id, true, NULL, this);
+                return;
             // Beacon of Light
             case 53563:
                 // original caster must be target (beacon)
