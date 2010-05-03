abf44569ac9468547d636f454513866af2b69b34
Insider <insider42rus@yandex.ru>
2010-05-03 15:36:35 +0700

    Small fix for [patch 15]

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7747,7 +7747,7 @@ bool Unit::HandleProcTriggerSpell(Unit *pVictim, uint32 damage, Aura* triggeredB
             // Unyielding Knights
             else if (auraSpellInfo->Id == 38164)
             {
-                if (pVictim->GetEntry() != 19457)
+                if (pVictim->GetTypeId() == TYPEID_PLAYER || pVictim->GetEntry() != 19457)
                     return false;
             }
             // Bonus Healing (Crystal Spire of Karabor mace)
