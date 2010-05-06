3546c489cc70aa2d856a22bd7a419e6b2ceee7f2
Insider <insider42rus@yandex.ru>
2010-05-06 18:19:21 +0700

    Reverted [patch 132]

--- addition/131_characters_saved_variables.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `saved_variables` ADD COLUMN `NextRandomBGResetTime` bigint(40) unsigned NOT NULL default '0' AFTER `NextWeeklyQuestResetTime`;
--- src/game/World.h
+++ src/game/World.h
@@ -182,7 +182,6 @@ enum eConfigUInt32Values
     CONFIG_UINT32_CHARDELETE_KEEP_DAYS,
     CONFIG_UINT32_CHARDELETE_METHOD,
     CONFIG_UINT32_CHARDELETE_MIN_LEVEL,
-    CONFIG_UINT32_RANDOM_BG_RESET_HOUR,
     CONFIG_UINT32_VALUE_COUNT
 };
 
@@ -497,7 +496,6 @@ class World
         /// Next daily quests reset time
         time_t GetNextDailyQuestsResetTime() const { return m_NextDailyQuestReset; }
         time_t GetNextWeeklyQuestsResetTime() const { return m_NextWeeklyQuestReset; }
-        time_t GetNextRandomBGResetTime() const { return m_NextRandomBGReset; }
 
         /// Get the maximum skill level a player can reach
         uint16 GetConfigMaxSkillValue() const
@@ -611,10 +609,8 @@ class World
 
         void InitDailyQuestResetTime();
         void InitWeeklyQuestResetTime();
-        void InitRandomBGResetTime();
         void ResetDailyQuests();
         void ResetWeeklyQuests();
-        void ResetRandomBG();
     private:
         void setConfig(eConfigUInt32Values index, char const* fieldname, uint32 defvalue);
         void setConfig(eConfigInt32Values index, char const* fieldname, int32 defvalue);
@@ -697,7 +693,6 @@ class World
         // next daily quests reset time
         time_t m_NextDailyQuestReset;
         time_t m_NextWeeklyQuestReset;
-        time_t m_NextRandomBGReset;
 
         //Player Queue
         Queue m_QueuedPlayer;
