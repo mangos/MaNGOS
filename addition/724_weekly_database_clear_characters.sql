70edb8566c7f0c534e442293962be1cb45313d15
/dev/rsa <kris13@bk.ru>
2010-05-07 19:25:36 +0400

    Added cleanup (weekly) script to AHbot/AHbyer

---
+++ addition/724_weekly_database_clear_characters.sql
@@ -0,0 +1,23 @@
+DELETE FROM `characters`.`item_instance` WHERE 
+SUBSTRING_INDEX(SUBSTRING_INDEX(`characters`.`item_instance`.`data`,' ',4),' ',-1) IN 
+(SELECT `mangos`.`item_template`.`entry`
+FROM `mangos`.`item_template` 
+WHERE `mangos`.`item_template`.`name` LIKE "% test%");
+
+DELETE FROM `characters`.`auctionhouse` USING `characters`.`auctionhouse` LEFT JOIN `characters`.`item_instance` ON `characters`.`auctionhouse`.`itemguid` = `characters`.`item_instance`.`guid` WHERE `characters`.`item_instance`.`guid` IS NULL;
+DELETE FROM `characters`.`auctionhouse` USING `characters`.`auctionhouse` LEFT JOIN `mangos`.`item_template` ON `auctionhouse`.`item_template` = `mangos`.`item_template`.`entry` WHERE `mangos`.`item_template`.`entry` IS NULL;
+
+LOCK TABLES `item_instance` WRITE ;
+START TRANSACTION;
+DELETE
+FROM `item_instance` 
+WHERE `guid` NOT IN (SELECT `item` FROM `character_inventory`) 
+AND `guid` NOT IN (SELECT `itemguid` FROM `auctionhouse`) 
+AND `guid` NOT IN (SELECT `item_guid` FROM `guild_bank_item`) 
+AND `guid` NOT IN (SELECT `item_guid` FROM `mail_items`) 
+AND `guid` NOT IN (SELECT `item_guid` FROM `character_gifts`)
+;
+COMMIT;
+
+UNLOCK TABLES;
+
