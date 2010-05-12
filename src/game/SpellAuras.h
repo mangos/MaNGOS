5f8829a724934d576c91103ce76fa28e19963b75
Insider <insider42rus@yandex.ru>
2010-05-12 15:37:34 +0700

    Added new version of [patch 134]

--- src/game/SpellAuras.h
+++ src/game/SpellAuras.h
@@ -221,6 +221,7 @@ class MANGOS_DLL_SPEC Aura
         void HandleIgnoreUnitState(bool Apply, bool Real);
         void HandleModTargetArmorPct(bool Apply, bool Real);
         void HandleAuraModAllCritChance(bool Apply, bool Real);
+        void HandleAuraLinked(bool Apply, bool Real);
         void HandleAuraOpenStable(bool apply, bool Real);
         void HandleAllowOnlyAbility(bool Apply, bool Real);
 
