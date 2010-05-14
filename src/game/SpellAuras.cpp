f081e40f63a9592288319ba7a2acb93b03792106
Insider <insider42rus@yandex.ru>
2010-05-14 20:30:39 +0700

    [patch 157] Fixed spell 47977. Patch provided by me

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2369,6 +2369,9 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                             // not use ammo and not allow use
                             ((Player*)m_target)->RemoveAmmo();
                         return;
+                    case 47977:                             // Magic Broom
+                        Spell::SelectMountByAreaAndSkill(m_target, 42680, 42683, 42667, 42668, 0);
+                        return;
                     case 48025:                             // Headless Horseman's Mount
                         Spell::SelectMountByAreaAndSkill(m_target, 51621, 48024, 51617, 48023, 0);
                         return;
