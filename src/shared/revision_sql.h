c63ba08a1c57937f792024cc5b6a9810b7866e6a
VladimirMangos <vladimir@getmangos.com>
2010-05-15 11:36:55 +0400

    [9899] Fixed sql update for ptrev. commit.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_9849_01_characters_saved_variables"
- #define REVISION_DB_MANGOS "required_9891_02_mangos_creature_movement_scripts"
+ #define REVISION_DB_MANGOS "required_9899_01_mangos_spell_bonus_data"
  #define REVISION_DB_REALMD "required_9748_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__
