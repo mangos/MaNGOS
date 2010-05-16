c586e78a9858d585e755e9a5e9ce4698027173f8
Insider <insider42rus@yandex.ru>
2010-05-16 14:46:23 +0700

    Added new version of [patch 159]

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3105,6 +3105,9 @@ void Spell::update(uint32 difftime)
         {
             if(m_timer)
             {
+                if (!m_targets.getUnitTarget()->isVisibleForOrDetect(m_caster, m_caster, false))
+                    cancel();
+
                 if(difftime >= m_timer)
                     m_timer = 0;
                 else
