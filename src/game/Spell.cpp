ad81ab878b1394c8c77aa25ca6f42273b1f47f9b
Insider <insider42rus@yandex.ru>
2010-05-16 17:45:49 +0700

    Fixed crash related to [patch 159]

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3105,7 +3105,7 @@ void Spell::update(uint32 difftime)
         {
             if(m_timer)
             {
-                if (!m_targets.getUnitTarget()->isVisibleForOrDetect(m_caster, m_caster, false))
+                if (m_targets.getUnitTarget() && !m_targets.getUnitTarget()->isVisibleForOrDetect(m_caster, m_caster, false))
                     cancel();
 
                 if(difftime >= m_timer)
