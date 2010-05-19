c4c8b3f26a8ac14c7af2721182077883b26a50dd
zergtmn <zergtmn@fetmangos.com>
2010-05-19 05:14:22 +0400

    [9933] Call DealDamageMods for leech DoTs damage
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -7233,6 +7233,8 @@ void Aura::PeriodicTick()
             DETAIL_FILTER_LOG(LOG_FILTER_PERIODIC_AFFECTS, "PeriodicTick: %u (TypeId: %u) health leech of %u (TypeId: %u) for %u dmg inflicted by %u abs is %u",
                 GUID_LOPART(GetCasterGUID()), GuidHigh2TypeId(GUID_HIPART(GetCasterGUID())), m_target->GetGUIDLow(), m_target->GetTypeId(), pdamage, GetId(),absorb);
 
+            pCaster->DealDamageMods(m_target, pdamage, &absorb);
+
             pCaster->SendSpellNonMeleeDamageLog(m_target, GetId(), pdamage, GetSpellSchoolMask(GetSpellProto()), absorb, resist, false, 0, isCrit);
 
             float multiplier = GetSpellProto()->EffectMultipleValue[GetEffIndex()] > 0 ? GetSpellProto()->EffectMultipleValue[GetEffIndex()] : 1;
