3a46184c657db34f9c5a06fbc8d92adbaf463df1
Insider <insider42rus@yandex.ru>
2010-05-27 12:42:41 +0700

    Reverted [patch 114]

--- addition/114_mangos_spell_bonus_data.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_bonus_data` WHERE `entry` IN (55078,55095);
-INSERT INTO `spell_bonus_data` VALUES
-(55078,0,0,0.06325,'Death Knight - Blood Plague Dummy Proc'),
-(55095,0,0,0.06325,'Death Knight - Frost Fever');
