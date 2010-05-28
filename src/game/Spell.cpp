7f7d8dcb8158990909ae70c9114a1af5f2a851e8
MaxXx2021 <MaxXx2021@ru-mangos-ru>
2010-05-28 14:35:47 +0400

    [9993] Limit spell 50988, 59870 targets amounts to expected.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1448,6 +1448,8 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
                 case 31347:                                 // Doom TODO: exclude top threat target from target selection
                 case 33711:                                 // Murmur's Touch
                 case 38794:                                 // Murmur's Touch (h)
+                case 50988:                                 // Glare of the Tribunal (Halls of Stone)
+                case 59870:                                 // Glare of the Tribunal (h) (Halls of Stone)
                     unMaxTargets = 1;
                     break;
                 case 28542:                                 // Life Drain
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "9992"
+ #define REVISION_NR "9993"
 #endif // __REVISION_NR_H__
