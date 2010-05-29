035aaa89ef4c8a08c169dff1ded128a35160c338
Insider <insider42rus@yandex.ru>
2010-05-29 17:21:31 +0700

    [patch 190] Fixed spell 65547. Patch provided by me

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4668,7 +4668,7 @@ void Aura::HandleModMechanicImmunity(bool apply, bool /*Real*/)
         uint32 mechanic = 1 << (misc-1);
 
         //immune movement impairment and loss of control
-        if(GetId()==42292 || GetId()==59752 || GetId()==53490)
+        if(GetId()==42292 || GetId()==59752 || GetId()==53490 || GetId()==65547)
             mechanic=IMMUNE_TO_MOVEMENT_IMPAIRMENT_AND_LOSS_CONTROL_MASK;
 
         m_target->RemoveAurasAtMechanicImmunity(mechanic,GetId());
