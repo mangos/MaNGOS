4f710b302dbbc7161848b76daa6fe22686c43a5a
Insider <insider42rus@yandex.ru>
2010-05-29 16:22:04 +0700

    [patch 187] Fixed spells 49219,51459 and ranks. Patch provided by me

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7245,7 +7245,7 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
             if (dummySpell->SpellIconID == 2709)
             {
                 // only melee auto attack affected
-                if (!(procFlag & PROC_FLAG_SUCCESSFUL_MELEE_HIT))
+                if (!(procFlag & PROC_FLAG_SUCCESSFUL_MELEE_HIT) && procSpell->Id != 56815)
                     return false;
 
                 basepoints[0] = triggerAmount * damage / 100;
@@ -7381,6 +7381,10 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
             // Blood-Caked Blade
             if (dummySpell->SpellIconID == 138)
             {
+                // only melee auto attack affected
+                if (!(procFlag & PROC_FLAG_SUCCESSFUL_MELEE_HIT) && procSpell->Id != 56815)
+                    return false;
+
                 triggered_spell_id = dummySpell->EffectTriggerSpell[effIndex];
                 break;
             }
