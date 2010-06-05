b6b043ad87e7ab66308791ba077ba285a2bf5c80
Insider <insider42rus@yandex.ru>
2010-06-05 17:19:41 +0700

    Added new version of [patch 157]

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2327,6 +2327,9 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                             // not use ammo and not allow use
                             ((Player*)target)->RemoveAmmo();
                         return;
+                    case 47977:                             // Magic Broom
+                        Spell::SelectMountByAreaAndSkill(target, 42680, 42683, 42667, 42668, 0);
+                        return;
                     case 48025:                             // Headless Horseman's Mount
                         Spell::SelectMountByAreaAndSkill(target, 51621, 48024, 51617, 48023, 0);
                         return;
