a61fd779260965f4735c0a856044da4d7d5ce331
Insider <insider42rus@yandex.ru>
2010-06-05 16:06:42 +0700

    Reverted [patch 131]

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6069,13 +6069,9 @@ void Aura::HandleShapeshiftBoosts(bool apply)
             break;
         case FORM_SHADOW:
             spellId1 = 49868;
-            spellId2 = 71167;
 
-            if(m_target->GetTypeId() == TYPEID_PLAYER)      // Spell 49868 and 71167 have same category as main form spell and share cooldown
-            {
+            if(m_target->GetTypeId() == TYPEID_PLAYER)      // Spell 49868 have same category as main form spell and share cooldown
                 ((Player*)m_target)->RemoveSpellCooldown(49868);
-                ((Player*)m_target)->RemoveSpellCooldown(71167);
-            }
             break;
         case FORM_GHOSTWOLF:
             spellId1 = 67116;
