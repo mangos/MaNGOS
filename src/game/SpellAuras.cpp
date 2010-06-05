d63b288dff1afeccbceab56a494636e573b6b5eb
Insider <insider42rus@yandex.ru>
2010-06-05 16:05:36 +0700

    Reverted [patch 65]

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6817,6 +6817,9 @@ void Aura::HandleAuraUntrackable(bool apply, bool /*Real*/)
 
 void Aura::HandleAuraModPacify(bool apply, bool /*Real*/)
 {
+    if(m_target->GetTypeId() != TYPEID_PLAYER)
+        return;
+
     if(apply)
         m_target->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PACIFIED);
     else
