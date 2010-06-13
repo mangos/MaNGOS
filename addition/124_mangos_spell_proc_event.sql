8025a5cc3306151ba24064eb7114692d3f2b052f
Insider <insider42rus@yandex.ru>
2010-06-13 14:54:29 +0700

    Small cleanup for [patch 99] and [patch 124]

--- addition/124_mangos_spell_proc_event.sql
+++ addition/124_mangos_spell_proc_event.sql
@@ -1,4 +1,3 @@
 DELETE FROM `spell_proc_event` WHERE `entry` IN (51692,51696);
 INSERT INTO `spell_proc_event` VALUES
-(51692,0,8,516,0,0,0,0,0,0,0),
-(51696,0,8,516,0,0,0,0,0,0,0);
+(51692,0,8,516,0,0,0,0,0,0,0);
