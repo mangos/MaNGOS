afb1d10ff8ad7483cf29ae98f67827bbeb399d2c
Insider <insider42rus@yandex.ru>
2010-06-14 14:56:30 +0700

    Reverted [patch 84]

--- src/game/Object.h
+++ src/game/Object.h
@@ -65,7 +65,6 @@ class WorldPacket;
 class UpdateData;
 class WorldSession;
 class Creature;
-class GameObject;
 class Player;
 class Unit;
 class Map;
@@ -478,7 +477,6 @@ class MANGOS_DLL_SPEC WorldObject : public Object
         void BuildUpdateData(UpdateDataMapType &);
 
         Creature* SummonCreature(uint32 id, float x, float y, float z, float ang,TempSummonType spwtype,uint32 despwtime);
-        GameObject* SummonGameobject(uint32 id, float x, float y, float z, float angle, uint32 despwtime);
     protected:
         explicit WorldObject();
 
