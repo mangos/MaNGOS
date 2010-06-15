eb4f2cd063b954b37d8dfd776f8c6a2f8064bffe
Insider <insider42rus@yandex.ru>
2010-06-15 14:45:08 +0700

    Fixed typo in [patch 159]

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3155,7 +3155,7 @@ void Spell::update(uint32 difftime)
         {
             if(m_timer)
             {
-                if (m_targets.getUnitTarget() && m_targets.getUnitTarget()->IsAlive() && !m_targets.getUnitTarget()->isVisibleForOrDetect(m_caster, m_caster, false))
+                if (m_targets.getUnitTarget() && m_targets.getUnitTarget()->isAlive() && !m_targets.getUnitTarget()->isVisibleForOrDetect(m_caster, m_caster, false))
                     cancel();
 
                 if(difftime >= m_timer)
