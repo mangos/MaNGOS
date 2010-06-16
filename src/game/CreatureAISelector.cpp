54e247ffec790cd8502c2b201a7da19573286633
VladimirMangos <vladimir@getmangos.com>
2010-06-17 02:50:52 +0400

    [10069] Really do "Avoid assign random move to player owned creatures as default movegen."

--- src/game/CreatureAISelector.cpp
+++ src/game/CreatureAISelector.cpp
@@ -91,7 +91,7 @@ namespace FactorySelector
         MovementGeneratorRegistry &mv_registry(MovementGeneratorRepository::Instance());
         ASSERT( creature->GetCreatureInfo() != NULL );
         MovementGeneratorCreator const * mv_factory = mv_registry.GetRegistryItem(
-            creature->GetOwnerGUID() ? FOLLOW_MOTION_TYPE : creature->GetDefaultMovementType());
+            IS_PLAYER_GUID(creature->GetOwnerGUID()) ? FOLLOW_MOTION_TYPE : creature->GetDefaultMovementType());
 
         /* if( mv_factory == NULL  )
         {
