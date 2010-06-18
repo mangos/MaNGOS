0273067164a83bba92dd4bd973eef1da0d8f8776
Insider <insider42rus@yandex.ru>
2010-06-18 21:22:44 +0700

    [patch 204] Fix: interrupt non melee spells at logout. Patch provided by zergtmn

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -403,6 +403,7 @@ void WorldSession::LogoutPlayer(bool Save)
 
         ///- Remove pet
         _player->RemovePet(NULL, PET_SAVE_AS_CURRENT, true);
+        _player->InterruptNonMeleeSpells(true);
 
         ///- empty buyback items and save the player in the database
         // some save parts only correctly work in case player present in map/player_lists (pets, etc)
