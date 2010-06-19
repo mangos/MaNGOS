7d17e773ac57d49a3255a8d1400fdf3cf08a79d7
Insider <insider42rus@yandex.ru>
2010-06-19 19:49:15 +0700

    Update for [patch 198]. Should restore work of barbershop

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -20715,7 +20715,7 @@ uint32 Player::GetBarberShopCost(uint8 newhairstyle, uint8 newhaircolor, uint8 n
     uint8 skintone = GetByteValue(PLAYER_BYTES, 0);
 
     if((hairstyle == newhairstyle) && (haircolor == newhaircolor) && (facialhair == newfacialhair) &&
-       (skintone == newskintone))
+        ((skintone == newskintone) || (newskintone == -1)))
         return 0;
 
     GtBarberShopCostBaseEntry const *bsc = sGtBarberShopCostBaseStore.LookupEntry(level - 1);
@@ -20734,8 +20734,8 @@ uint32 Player::GetBarberShopCost(uint8 newhairstyle, uint8 newhaircolor, uint8 n
     if(facialhair != newfacialhair)
         cost += bsc->cost * 0.75f;                          // +3/4 of price
 
-    if(skintone != newskintone)
-        cost += bsc->cost * 0.5f;                           // +1/2 of price
+    if(skintone != newskintone && newskintone != -1)        // +1/2 of price
+        cost += bsc->cost * 0.5f;
 
     return uint32(cost);
 }
