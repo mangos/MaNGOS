8e9c813c670fb372a5f3b3e47de4bb93ad0b99fe
tomrus88 <tomrus88@gmail.com>
2010-06-29 18:28:30 +0400

    Damage correction for high item levels

--- src/game/ItemPrototype.h
+++ src/game/ItemPrototype.h
@@ -645,8 +645,8 @@ struct ItemPrototype
     }
 
     uint32 GetArmor() const;
-    float GetMinDamage() const { return floor(getDPS() * float(Delay) / 1000.0f * 0.8f + 0.5f); }
-    float GetMaxDamage() const { return floor(getDPS() * float(Delay) / 1000.0f * 1.2f + 0.5f); }
+    float GetMinDamage() const { return floor(getDPS() * float(Delay) / 1000.0f * 0.7f + 0.5f); }
+    float GetMaxDamage() const { return floor(getDPS() * float(Delay) / 1000.0f * 1.3f + 0.5f); }
 
     bool IsPotion() const { return Class==ITEM_CLASS_CONSUMABLE && SubClass==ITEM_SUBCLASS_POTION; }
     bool IsConjuredConsumable() const { return Class == ITEM_CLASS_CONSUMABLE && (Flags & ITEM_FLAGS_CONJURED); }
