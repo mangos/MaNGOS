f7cac4136f6074f6356634c94efef0c1140bdcf6
NoFantasy <nofantasy@nf.no>
2010-07-11 10:58:25 +0200

    [10177] Add custom code for triggered part of spell 39105
    Signed-off-by: NoFantasy <nofantasy@nf.no>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1421,8 +1421,13 @@ void Aura::TriggerSpell()
 //                    case 38751: break;
 //                    // Murmur's Touch
 //                    case 38794: break;
-//                    // Activate Nether-wraith Beacon (31742 Nether-wraith Beacon item)
-//                    case 39105: break;
+                    case 39105:                             // Activate Nether-wraith Beacon (31742 Nether-wraith Beacon item)
+                    {
+                        float fX, fY, fZ;
+                        triggerTarget->GetClosePoint(fX, fY, fZ, triggerTarget->GetObjectBoundingRadius(), 20.0f);
+                        triggerTarget->SummonCreature(22408, fX, fY, fZ, triggerTarget->GetOrientation(), TEMPSUMMON_DEAD_DESPAWN, 0);
+                        return;
+                    }
 //                    // Drain World Tree Visual
 //                    case 39140: break;
 //                    // Quest - Dustin's Undead Dragon Visual aura
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "10176"
+ #define REVISION_NR "10177"
 #endif // __REVISION_NR_H__
