b21a2120362484c6f6c1f6d1cabfdd43a10f13b5
selector <limurchick@gmail.com>
2010-07-13 15:06:27 +0200

    [C80] Fix Icecrown healers trinket target selection
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2048,6 +2048,11 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
 
                 FillRaidOrPartyHealthPriorityTargets(targetUnitMap, m_caster, target, radius, count, true, false, true);
             }
+            // Item - Icecrown 25 Heroic/Normal Healer Trinket 2
+            else if (m_spellInfo->Id == 71641 || m_spellInfo->Id == 71610)
+            {
+                FillRaidOrPartyHealthPriorityTargets(targetUnitMap, m_caster, m_caster, radius, 1, true, false, false);
+            }
             else
                 FillAreaTargets(targetUnitMap, m_targets.m_destX, m_targets.m_destY, radius, PUSH_DEST_CENTER, SPELL_TARGETS_FRIENDLY);
             break;
