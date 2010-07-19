72e1253a88459b5b5e805b66d088dba25dbe66eb
Lutik <Lutik@getmangos.com>
2010-07-19 11:41:24 +0400

    [10226] Fixed GO_JUST_DEACTIVATED for despawned by default gameobject.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -417,14 +417,8 @@ void GameObject::Update(uint32 /*p_time*/)
             if(!m_respawnDelayTime)
                 return;
 
-            if(!m_spawnedByDefault)
-            {
-                m_respawnTime = 0;
-                return;
-            }
-
             // since pool system can fail to roll unspawned object, this one can remain spawned, so must set respawn nevertheless
-            m_respawnTime = time(NULL) + m_respawnDelayTime;
+            m_respawnTime = m_spawnedByDefault ? time(NULL) + m_respawnDelayTime : 0;
 
             // if option not set then object will be saved at grid unload
             if(sWorld.getConfig(CONFIG_BOOL_SAVE_RESPAWN_TIME_IMMEDIATLY))
