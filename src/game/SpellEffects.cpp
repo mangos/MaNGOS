e0dfcecab51e2ec8b44058f99e8a2bd3513eeb10
selector <limurchick@gmail.com>
2010-07-20 22:19:28 +0300

    [S013] Fix summon types 2081 and 2141
    author: zergtmn

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -4547,6 +4547,19 @@ void Spell::DoSummonGuardian(SpellEffectIndex eff_idx, uint32 forceFaction)
 
     int32 amount = damage > 0 ? damage : 1;
 
+    switch(m_spellInfo->EffectMiscValueB[eff_idx])
+    {
+        case 2081: // Engineering Dragonlings
+        case 2141: // Winterfin Horn of Distress
+        {
+            if (level > m_spellInfo->maxLevel)
+                level = m_spellInfo->maxLevel;
+
+            amount = 1;
+            break;
+        }
+    }
+
     for(int32 count = 0; count < amount; ++count)
     {
         Pet* spawnCreature = new Pet(GUARDIAN_PET);
