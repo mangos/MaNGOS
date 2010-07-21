9898741626e3fa56dd68ee4d009da804b1c8c296
Lynx3d <lynx3d@some-imaginary-isp.org>
2010-07-21 03:33:22 +0200

    [10239] Fix mangos.sql
    I just had to forget one thing...

--- sql/mangos.sql
+++ sql/mangos.sql
@@ -14258,6 +14258,7 @@ INSERT INTO `spell_bonus_data` VALUES
 (8188,  0.1,    0,       0,     'Shaman - Magma Totam Passive'),
 (61295, 0.4,    0.18,    0,     'Shaman - Riptide'),
 (3606,  0.1667, 0,       0,     'Shaman - Searing Totem Attack'),
+(10444, 0,      0,       0,     'Shaman - Flametongue Attack'),
 /* Warlock */
 (17962, 0,      0,       0,     'Warlock - Conflagrate'),
 (172,   0,      0.2,     0,     'Warlock - Corruption'),
