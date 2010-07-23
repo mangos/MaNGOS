80f7c8761f2a700e2c88871f3c7c15b709d9571c
Laise <fenrisse@gmail.com>
2010-07-23 09:21:25 +0300

    [10249] Don't interrupt area effects for channeled spells from target side

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4575,7 +4575,7 @@ void Unit::RemoveSpellAuraHolder(SpellAuraHolder *holder, AuraRemoveMode mode)
     else
         delete holder;
 
-    if (mode != AURA_REMOVE_BY_EXPIRE && IsChanneledSpell(AurSpellInfo) && caster && caster->GetGUID() != GetGUID())
+    if (mode != AURA_REMOVE_BY_EXPIRE && IsChanneledSpell(AurSpellInfo) && !IsAreaOfEffectSpell(AurSpellInfo) && caster && caster->GetGUID() != GetGUID())
         caster->InterruptSpell(CURRENT_CHANNELED_SPELL);
 }
 
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "10248"
+ #define REVISION_NR "10249"
 #endif // __REVISION_NR_H__
