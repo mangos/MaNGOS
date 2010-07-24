b6bcb21563c3c6706a65f88575c92066d5ff2dbe
Laise <fenrisse@gmail.com>
2010-07-24 17:07:12 +0300

    [10259] Fix some iterator in Aura::HandleAuraModShapeshift

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2814,7 +2814,7 @@ void Aura::HandleAuraModShapeshift(bool apply, bool Real)
             // remove movement affects
             target->RemoveSpellsCausingAura(SPELL_AURA_MOD_ROOT);
             Unit::AuraList const& slowingAuras = target->GetAurasByType(SPELL_AURA_MOD_DECREASE_SPEED);
-            for (Unit::AuraList::const_iterator iter = slowingAuras.begin(); iter != slowingAuras.end();)
+            for (Unit::AuraList::iterator iter = slowingAuras.begin(); iter != slowingAuras.end();)
             {
                 SpellEntry const* aurSpellInfo = (*iter)->GetSpellProto();
 
