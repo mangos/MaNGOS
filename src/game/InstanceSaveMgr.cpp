91a1a8c9a446af7251af019f503ef543b98f6e17
/dev/rsa <kris13@bk.ru>
2010-07-29 18:39:25 +0400

    Reset time for instances cannot be more than 7 days (hack for ruby_sanctum, v0.2)

--- src/game/InstanceSaveMgr.cpp
+++ src/game/InstanceSaveMgr.cpp
@@ -204,6 +204,7 @@ void InstanceResetScheduler::LoadResetTimes()
 
             // update the reset time if the hour in the configs changes
             uint64 newresettime = (oldresettime / DAY) * DAY + diff;
+            newresettime = newresettime >= today + 8*DAY ? today + 6*DAY + diff : newresettime;
             if(oldresettime != newresettime)
                 CharacterDatabase.DirectPExecute("UPDATE instance_reset SET resettime = '"UI64FMTD"' WHERE mapid = '%u' AND difficulty = '%u'", newresettime, mapid, difficulty);
 
@@ -234,12 +235,17 @@ void InstanceResetScheduler::LoadResetTimes()
         if (period < DAY)
             period = DAY;
 
+        // the reset_delay must be not more 7 days
+        if (period > 7*DAY)
+            period = 7*DAY;
+
         time_t t = GetResetTimeFor(mapid,difficulty);
         if(!t)
         {
             // initialize the reset time
             t = today + period + diff;
             CharacterDatabase.DirectPExecute("INSERT INTO instance_reset VALUES ('%u','%u','"UI64FMTD"')", mapid, difficulty, (uint64)t);
+
         }
 
         if(t < now)
