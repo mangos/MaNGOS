0ae155247818a08c2646512f76051ec08e08c7d9
Janu <jaed1@gmx.net>
2010-07-31 18:57:43 +0200

    [J003] allow use of GetSpellEntryByDifficulty in scripts
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -516,7 +516,7 @@ bool IsDiminishingReturnsGroupDurationLimited(DiminishingGroup group);
 DiminishingReturnsType GetDiminishingReturnsGroupType(DiminishingGroup group);
 int32 GetDiminishingReturnsLimitDuration(DiminishingGroup group, SpellEntry const* spellproto);
 
-SpellEntry const* GetSpellEntryByDifficulty(uint32 id, Difficulty difficulty);
+MANGOS_DLL_SPEC SpellEntry const* GetSpellEntryByDifficulty(uint32 id, Difficulty difficulty);
 
 // Spell proc event related declarations (accessed using SpellMgr functions)
 enum ProcFlags
