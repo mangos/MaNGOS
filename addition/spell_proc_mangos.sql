1f7496b3fbcf086bbe6caaa10e1ef2547a07fadc
/dev/rsa <kris13@bk.ru>
2010-08-23 16:34:00 +0400

    Correct sql's

--- addition/spell_proc_mangos.sql
+++ addition/spell_proc_mangos.sql
@@ -180,3 +180,11 @@ INSERT INTO spell_proc_event (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFa
 DELETE FROM `spell_proc_event` WHERE `entry` = 70756;
 INSERT INTO `spell_proc_event` VALUES ('70756', '2', '10', '0x00200000', '0x00200000', '0x00200000', '0', '0', '0', '0', '0', '0', '16384', '0', '0', '0', '0');
 
+DELETE FROM `spell_proc_event` WHERE `entry` IN ('71519', '71562');
+INSERT INTO `spell_proc_event` VALUES ('71562', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '60');
+INSERT INTO `spell_proc_event` VALUES ('71519', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '60');
+
+DELETE FROM `spell_proc_event` WHERE `entry` IN ('70723', '70808');
+INSERT INTO `spell_proc_event` VALUES ('70723','72', '7', '5','5', '5', '0', '0','0', '0', '0', '0', '65536', '2', '0', '0', '0');
+INSERT INTO `spell_proc_event` VALUES ('70808','8', '11', '400', '400', '400', '0', '0', '0', '0', '0', '0', '0', '2', '0', '0', '0');
+
