49a5461d5fbc6f5bca8eee1a3c2860282eba1c41
Wowka321 <galaxys@inbox.ru>
2010-08-24 14:04:09 +0300

    Not break stealth for some cases

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1216,7 +1216,7 @@ void Spell::DoSpellHitOnUnit(Unit *unit, const uint32 effectMask)
             }
 
             // not break stealth by cast targeting
-            if (!(m_spellInfo->AttributesEx & SPELL_ATTR_EX_NOT_BREAK_STEALTH))
+            if (!(m_spellInfo->AttributesEx & SPELL_ATTR_EX_NOT_BREAK_STEALTH) && m_spellInfo->Id != 51690 && m_spellInfo->Id != 53055)
                 unit->RemoveSpellsCausingAura(SPELL_AURA_MOD_STEALTH);
 
             // can cause back attack (if detected), stealth removed at Spell::cast if spell break it
