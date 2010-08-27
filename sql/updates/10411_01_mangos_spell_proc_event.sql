de0c63ff3bf4ebb158f4a7fe289ad1a92755670d
Laise <fenrisse@gmail.com>
2010-08-27 19:11:49 +0300

    [10411] fix broken sqls for spell_proc_event..

---
+++ sql/updates/10411_01_mangos_spell_proc_event.sql
@@ -0,0 +1,3 @@
+ALTER TABLE db_version CHANGE COLUMN required_10410_01_mangos_spell_chain required_10411_01_mangos_spell_proc_event bit;
+
+insert into spell_proc_event values (47230, 0x7F,  5, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0.000000, 0.000000,  0);
