21614eb8daa4f9a2faf7ae8e92d2dd859bd08972
Laise <fenrisse@gmail.com>
2010-08-28 09:28:53 +0300

    [10416] fix 71611 proc and remove check for family name if no spell_proc_event is defined

---
+++ sql/updates/10416_01_mangos_spell_proc_event.sql
@@ -0,0 +1,4 @@
+ALTER TABLE db_version CHANGE COLUMN required_10411_01_mangos_spell_proc_event required_10416_01_mangos_spell_proc_event bit;
+
+DELETE FROM spell_proc_event WHERE entry = 71611;
+INSERT INTO spell_proc_event VALUES (71611, 0x7F,  0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0.000000, 0.000000,  0);
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "10415"
+ #define REVISION_NR "10416"
 #endif // __REVISION_NR_H__
