655584fe468a118c41f1d5671972c41c6eafaccd
/dev/rsa <kris13@bk.ru>
2010-09-11 19:15:42 +0400

    SQL for vehicles split

--- addition/010_S100_mangos_multiseat_mounts.sql
+++
@@ -1,9 +0,0 @@
--- Mechano-hog, Mekgineer's Chopper
-UPDATE `creature_template` SET `VehicleId` = 318, IconName = 'vehichleCursor' WHERE `entry` IN (29929, 32286);
--- Traveler's Tundra Mammoth
--- Grand Ice Mammoth
--- Grand Black War Mammoth
--- Grand Caravan Mammoth
-UPDATE `creature_template` SET `VehicleId` = 312, IconName = 'vehichleCursor' WHERE `entry` IN (32633, 32640, 31857, 31858, 31861, 31862, 32212, 32213);
--- X-53 Touring Rocket
-UPDATE `creature_template` SET `VehicleId` = 774, IconName = 'vehichleCursor' WHERE `entry` = 40725;
