af4105c90ba338851c914958407398b7dbdb0ec4
/dev/rsa <kris13@bk.ru>
2010-09-17 15:01:53 +0400

    SQL corrects, thanks to jahangames && virusaw

--- addition/732_quests_vehicle_data_mangos.sql
+++ addition/732_quests_vehicle_data_mangos.sql
@@ -21,4 +21,51 @@ INSERT INTO npc_spellclick_spells VALUES
 (28607, 52263, 12680, 1, 12680, 1);
 INSERT IGNORE INTO spell_script_target VALUES (52264, 1, 28653);
 
---
+-- From jahangames
+-- Into the realm of Shadows quest
+UPDATE creature_template SET
+spell1 = 52362,
+spell2 = 0,
+spell3 = 0,
+spell4 = 0,
+spell5 = 0,
+spell6 = 0,
+VehicleId = 135
+WHERE entry IN (28782);
+
+DELETE FROM npc_spellclick_spells WHERE npc_entry in (28782);
+INSERT INTO npc_spellclick_spells VALUES (28782, 52349, 12687, 1, 12687, 1);
+INSERT IGNORE INTO spell_script_target VALUES (52349, 1, 28782);
+DELETE FROM creature_involvedrelation WHERE quest in (12687);
+INSERT INTO creature_involvedrelation (id, quest) VALUES (28788, 12687);
+UPDATE creature_template SET npcflag=npcflag|2 WHERE entry=28788;
+UPDATE quest_template SET SpecialFlags = 0 , SuggestedPlayers = 0 , Method = 1 WHERE entry IN (12687);
+
+-- Massacre at Light's point quest
+UPDATE creature_template SET
+spell1 = 52435,
+spell2 = 52576,
+spell3 = 0,
+spell4 = 0,
+spell5 = 52588,
+spell6 = 0,
+VehicleId = 79
+WHERE entry IN (28833);
+
+UPDATE creature_template SET
+spell1 = 52435,
+spell2 = 52576,
+spell3 = 0,
+spell4 = 0,
+spell5 = 52588,
+spell6 = 0,
+VehicleId = 68
+WHERE entry IN (28887);
+
+INSERT INTO npc_spellclick_spells VALUES ('28833', '52447', '12701', '1', '12701', '1');
+INSERT INTO npc_spellclick_spells VALUES ('28887', '52447', '12701', '1', '12701', '1');
+UPDATE creature_template SET minhealth = 26140, maxhealth = 26140, dynamicflags = 0, minmana = 2117, maxmana = 2117, unit_flags = 772, minlevel = 55, maxlevel = 55, unk16 = 10, unk17 = 1, InhabitType = 3, scale = 1, mindmg = 685, maxdmg = 715, armor = 3232, attackpower = 214, unit_class = 2, type = 10 WHERE entry = 28833;
+UPDATE creature_template SET minhealth = 26140, maxhealth = 26140, dynamicflags = 0, minmana = 0, maxmana = 0, unit_flags = 772, minlevel = 55, maxlevel = 55, unk16 = 10, unk17 = 1, InhabitType = 3, scale = 1, mindmg = 685, maxdmg = 715, armor = 3232, attackpower = 214, unit_class = 2, type = 10 WHERE entry = 28887;
+INSERT IGNORE INTO spell_script_target VALUES (52576, 1, 28834);
+INSERT IGNORE INTO spell_script_target VALUES (52576, 1, 28886);
+INSERT IGNORE INTO spell_script_target VALUES (52576, 1, 28850);
