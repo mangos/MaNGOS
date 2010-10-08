507b0a50ce68b9ecef53db141b3dd3481f95e000
/dev/rsa <kris13@bk.ru>
2010-10-08 19:43:30 +0400

    attempt to fix crush in HandleQuestgiverStatusMultipleQuery

--- src/game/QuestHandler.cpp
+++ src/game/QuestHandler.cpp
@@ -663,7 +663,7 @@ void WorldSession::HandleQuestgiverStatusMultipleQuery(WorldPacket& /*recvPacket
             // need also pet quests case support
             Creature *questgiver = GetPlayer()->GetMap()->GetAnyTypeCreature(*itr);
 
-            if (!questgiver || questgiver->IsHostileTo(_player))
+            if (!questgiver ||questgiver->isPet() || questgiver->IsHostileTo(_player))
                 continue;
 
             if (!questgiver->HasFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_QUESTGIVER))
