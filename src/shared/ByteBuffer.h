0daf0ec82471c740829c90148d95b8679d67da28
DasBlub <dasblub@gmail.com>
2010-10-16 15:35:44 +0200

    [10613] A bit of cleanup in ByteBuffer::hexlike

--- src/shared/ByteBuffer.h
+++ src/shared/ByteBuffer.h
@@ -446,48 +446,23 @@ class ByteBuffer
             {
                 if ((i == (j * 8)) && ((i != (k * 16))))
                 {
-                    if (read<uint8>(i) < 0x10)
-                    {
-                        sLog.outDebugInLine("| 0%X ", read<uint8>(i) );
-                    }
-                    else
-                    {
-                        sLog.outDebugInLine("| %X ", read<uint8>(i) );
-                    }
+                    sLog.outDebugInLine("| %02X ", read<uint8>(i));
                     ++j;
                 }
                 else if (i == (k * 16))
                 {
-                    if (read<uint8>(i) < 0x10)
-                    {
-                        sLog.outDebugInLine("\n");
-                        if(sLog.IsIncludeTime())
-                            sLog.outDebugInLine("         ");
+                    sLog.outDebugInLine("\n");
+                    if(sLog.IsIncludeTime())
+                        sLog.outDebugInLine("         ");
 
-                        sLog.outDebugInLine("0%X ", read<uint8>(i) );
-                    }
-                    else
-                    {
-                        sLog.outDebugInLine("\n");
-                        if(sLog.IsIncludeTime())
-                            sLog.outDebugInLine("         ");
-
-                        sLog.outDebugInLine("%X ", read<uint8>(i) );
-                    }
+                    sLog.outDebugInLine("%02X ", read<uint8>(i));
 
                     ++k;
                     ++j;
                 }
                 else
                 {
-                    if (read<uint8>(i) < 0x10)
-                    {
-                        sLog.outDebugInLine("0%X ", read<uint8>(i) );
-                    }
-                    else
-                    {
-                        sLog.outDebugInLine("%X ", read<uint8>(i) );
-                    }
+                    sLog.outDebugInLine("%02X ", read<uint8>(i));
                 }
             }
             sLog.outDebugInLine("\n");
