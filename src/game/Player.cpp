ed4223bc300d37a481bb78083e6bc214a86e3fe9
NoFantasy <nofantasy@nf.no>
2010-10-19 11:57:49 +0200

    [10624] Fix quests w/QuestFlags QUEST_FLAGS_AUTO_REWARDED
    Signed-off-by: NoFantasy <nofantasy@nf.no>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -13324,6 +13324,17 @@ bool Player::CanCompleteQuest( uint32 quest_id ) const
     if (!qInfo)
         return false;
 
+    // only used for "flag" quests and not real in-game quests
+    if (qInfo->HasFlag(QUEST_FLAGS_AUTO_REWARDED))
+    {
+        // a few checks, not all "satisfy" is needed
+        if (SatisfyQuestPreviousQuest(qInfo, false) && SatisfyQuestLevel(qInfo, false) &&
+            SatisfyQuestSkill(qInfo, false) && SatisfyQuestRace(qInfo, false) && SatisfyQuestClass(qInfo, false))
+            return true;
+
+        return false;
+    }
+
     // auto complete quest
     if (qInfo->IsAutoComplete() && CanTakeQuest(qInfo, false))
         return true;
