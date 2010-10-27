67919f4354c02694ec3596775281a5816040716b
Insider <insider42rus@yandex.ru>
2010-10-27 19:55:46 +0700

    [patch 125] Fixed second part of spell 57499. Patch provided by me

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -5153,7 +5153,7 @@ SpellCastResult Spell::CheckCast(bool strict)
             }
             case SPELL_EFFECT_CHARGE:
             {
-                if (m_caster->hasUnitState(UNIT_STAT_ROOT))
+                if (m_caster->hasUnitState(UNIT_STAT_ROOT) && !m_caster->HasAura(57499))
                     return SPELL_FAILED_ROOTED;
 
                 break;
