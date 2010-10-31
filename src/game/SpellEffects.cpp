e516d0bf53c21b36a926f7c64a705c250ed3f141
mosst <Makcim_Chaos@Ukr.net>
2010-10-31 23:22:35 +0200

    [bws126] Fix spell 49941 and ranks - bonus for diseased targets.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -870,6 +870,16 @@ void Spell::EffectSchoolDMG(SpellEffectIndex effect_idx)
                 }
                 break;
             }
+            case SPELLFAMILY_DEATHKNIGHT:
+            {
+                // Blood Boil - bonus for diseased targets
+                if (m_spellInfo->SpellFamilyFlags & 0x00040000 && unitTarget->GetAura(SPELL_AURA_PERIODIC_DAMAGE, SPELLFAMILY_DEATHKNIGHT, 0, 0x00000002, m_caster->GetGUID()))
+                {
+                    damage += m_damage / 2;
+                    damage += int32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK)* 0.035f);
+                }
+                break;
+            }
         }
 
         if(damage >= 0)
