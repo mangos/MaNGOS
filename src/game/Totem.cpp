0db336e8d90f91ab6879fc19feeea44ce981f5ac
eggxp <eggxp@getmangos.com>
2010-11-01 02:25:10 +0300

    [10663] Fix totem animation at normal unsummon.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Totem.cpp
+++ src/game/Totem.cpp
@@ -106,6 +106,10 @@ void Totem::UnSummon()
             ((Creature*)owner)->AI()->SummonedCreatureDespawn((Creature*)this);
     }
 
+    // any totem unsummon look like as totem kill, req. for proper animation
+    if (isAlive())
+        SetDeathState(DEAD);
+
     AddObjectToRemoveList();
 }
 
