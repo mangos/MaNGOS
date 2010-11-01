c7d0362c6e85a32d801d9a83b2cb9ee4ec3a10fd
Ceris <Ceris@getmangos.com>
2010-11-01 07:51:39 +0300

    [10669] Fixed mana amount for overheal case for talent 31785 and ranks.
    Also thanks to Wojta for patch code improvements.
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -1873,8 +1873,14 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura
                     if (this == pVictim)
                         return SPELL_AURA_PROC_FAILED;
 
-                    // heal amount
-                    basepoints[0] = triggerAmount*damage/100;
+                    // dont count overhealing
+                    uint32 diff = GetMaxHealth()-GetHealth();
+                    if (!diff)
+                        return SPELL_AURA_PROC_FAILED;
+                    if (damage > diff)
+                        basepoints[0] = triggerAmount*diff/100;
+                    else
+                        basepoints[0] = triggerAmount*damage/100;
                     target = this;
                     triggered_spell_id = 31786;
                     break;
