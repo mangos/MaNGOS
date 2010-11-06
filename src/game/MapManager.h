89dac0cbbfd5c8d21192d5341520578448fd7d63
VladimirMangos <vladimir@getmangos.com>
2010-11-06 21:54:05 +0300

    [10687] UInt32 timer class copies instead using time_t timers for mstime cases.
    Also cleanup weather update code and some random code cleanups.

--- src/game/MapManager.h
+++ src/game/MapManager.h
@@ -171,7 +171,7 @@ class MANGOS_DLL_DECL MapManager : public MaNGOS::Singleton<MapManager, MaNGOS::
         typedef MaNGOS::ClassLevelLockable<MapManager, ACE_Thread_Mutex>::Lock Guard;
         uint32 i_gridCleanUpDelay;
         MapMapType i_maps;
-        IntervalTimer i_timer;
+        ShortIntervalTimer i_timer;
 };
 
 #define sMapMgr MapManager::Instance()
