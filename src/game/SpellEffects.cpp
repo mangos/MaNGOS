ecb8e9d79464c63e2b9d7e5d564818b3ad8d9786
/dev/rsa <kris13@bk.ru>
2010-11-12 23:30:38 +0300

    corrects after last merge

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -7455,22 +7455,6 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
             }
             break;
         }
-       case SPELLFAMILY_WARRIOR:
-        {
-            switch(m_spellInfo->Id)
-            {
-                case 64380:                                 // Shattering Throw
-                {
-                    if (!unitTarget || !unitTarget->isAlive())
-                        return;
-
-                    // remove immunity effects
-                    unitTarget->RemoveAurasBySpellMechanic(MECHANIC_IMMUNE_SHIELD);
-                    break;
-                }
-            }
-            break;
-        }
         case SPELLFAMILY_WARRIOR:
         {
             switch(m_spellInfo->Id)
