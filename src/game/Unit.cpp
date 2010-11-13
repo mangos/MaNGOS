6dd31b1a6e917e85b4a025111cd660f0670692d4
/dev/rsa <kris13@bk.ru>
2010-11-13 17:46:17 +0300

    attempt to fix crushes/freezes in GetPet. Cheat result?

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6157,7 +6157,10 @@ Pet* Unit::GetPet() const
 
 Pet* Unit::_GetPet(ObjectGuid guid) const
 {
-    return GetMap()->GetPet(guid);
+    if (Map* pMap = GetMap())
+        return pMap->GetPet(guid);
+    else
+        return NULL;
 }
 
 void Unit::RemoveMiniPet()
