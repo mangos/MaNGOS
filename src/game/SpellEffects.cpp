dd8c84732df315e00a27b0dec0630dd1b6b56270
NoFantasy <nofantasy@nf.no>
2010-11-17 13:00:19 +0100

    [10729] Add dummy aura effect of spell 47178 and dummy effect of 47176
    Note: see comments in code for spell 47176
    
    Signed-off-by: NoFantasy <nofantasy@nf.no>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1404,6 +1404,17 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                     unitTarget->CastSpell(unitTarget,46798,true,m_CastItem,NULL,m_originalCasterGUID);
                     break;
                 }
+                case 47176:                                 // Infect Ice Troll
+                {
+                    // Spell has wrong areaGroupid, so it can not be casted where expected.
+                    // TODO: research if spells casted by NPC, having TARGET_SCRIPT, can have disabled area check
+                    if (!unitTarget)
+                        return;
+
+                    // Plague Effect Self
+                    unitTarget->CastSpell(unitTarget, 47178, true);
+                    return;
+                }
                 case 49357:                                 // Brewfest Mount Transformation
                 {
                     if (m_caster->GetTypeId() != TYPEID_PLAYER)
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "10728"
+ #define REVISION_NR "10729"
 #endif // __REVISION_NR_H__
