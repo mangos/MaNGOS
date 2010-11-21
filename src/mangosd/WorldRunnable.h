8ab5fa8580e8d89d1b6568043db39ce62fc1003f
VladimirMangos <vladimir@getmangos.com>
2010-11-22 01:29:58 +0300

    [10771] Explictly include Threading.h into WorldRunnable.h
    Instead dependent from includes order in mangosd\WorldRunnable.cpp and mangosd\Master.cpp

--- src/mangosd/WorldRunnable.h
+++ src/mangosd/WorldRunnable.h
@@ -23,6 +23,9 @@
 #ifndef __WORLDRUNNABLE_H
 #define __WORLDRUNNABLE_H
 
+#include "Common.h"
+#include "Threading.h"
+
 /// Heartbeat thread for the World
 class WorldRunnable : public ACE_Based::Runnable
 {
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "10770"
+ #define REVISION_NR "10771"
 #endif // __REVISION_NR_H__
