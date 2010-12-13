462ea0af7f249fef6eeb7e92155d7ba73f84245a
Feanordev <Feanordev@gmail.com>
2010-12-13 21:21:17 +0100

    Send BG Status packet to all players at arena start (Arena UI Frame)

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -459,9 +459,19 @@ void BattleGround::Update(uint32 diff)
             {
                 //TODO : add arena sound PlaySoundToAll(SOUND_ARENA_START);
 
+                // remove auras with duration lower than 30s and arena preparation
                 for(BattleGroundPlayerMap::const_iterator itr = m_Players.begin(); itr != m_Players.end(); ++itr)
                     if (Player *plr = sObjectMgr.GetPlayer(itr->first))
+                    {
+                        // BG Status packet
+                        WorldPacket status;
+                        BattleGroundQueueTypeId bgQueueTypeId = BattleGroundMgr::BGQueueTypeId(m_TypeID, GetArenaType());
+                        uint32 queueSlot = plr->GetBattleGroundQueueIndex(bgQueueTypeId);
+                        sBattleGroundMgr.BuildBattleGroundStatusPacket(&status, this, queueSlot, GetStatus(), 0, GetStartTime(), GetArenaType());
+                        plr->GetSession()->SendPacket(&status);
+
                         plr->RemoveAurasDueToSpell(SPELL_ARENA_PREPARATION);
+                    }
 
                 CheckArenaWinConditions();
             }
