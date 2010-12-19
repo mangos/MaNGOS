551200a925bc679da2b97ec45213784773b4893d
zergtmn <zerg@myisp.com>
2010-12-19 22:36:36 +0500

    [10900] Remove SPELL_AURA_MOD_POSSESS_PET auras in Unit::Uncharm()

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5916,6 +5916,7 @@ void Unit::Uncharm()
     {
         charm->RemoveSpellsCausingAura(SPELL_AURA_MOD_CHARM);
         charm->RemoveSpellsCausingAura(SPELL_AURA_MOD_POSSESS);
+        charm->RemoveSpellsCausingAura(SPELL_AURA_MOD_POSSESS_PET);
     }
 }
 
