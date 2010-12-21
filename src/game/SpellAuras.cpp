1ffe4e37292af0d2021927b7d2ac4926ae44d670
/dev/rsa <kris13@bk.ru>
2010-12-21 08:26:19 +0300

    Blood oath from virusav

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2088,6 +2088,10 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                     case 48025:                             // Headless Horseman's Mount
                         Spell::SelectMountByAreaAndSkill(target, 51621, 48024, 51617, 48023, 0);
                         return;
+                    case 50141:                             // Blood Oath
+                        // Blood Oath
+                        target->CastSpell(target, 50001, true, NULL, this);
+                        return;
                     case 55328:                                 // Stoneclaw Totem I
                         target->CastSpell(target, 5728, true);
                         return;
