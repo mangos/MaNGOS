1dc54a8c05cba0458e2cca45a34e95b6321fdbee
Ambal <pogrebniak@gala.net>
2010-12-24 01:22:04 +0200

    [10914] Remove ObjectLevelLockable inheritance in Map class + cleanup locks in Map code. Restore build after recent commits.
    Signed-off-by: Ambal <pogrebniak@gala.net>

--- src/game/Map.h
+++ src/game/Map.h
@@ -35,6 +35,7 @@
 #include "GameSystem/GridRefManager.h"
 #include "MapRefManager.h"
 #include "Utilities/TypeList.h"
+#include "ScriptMgr.h"
 
 #include <bitset>
 #include <list>
@@ -46,7 +47,6 @@ class InstanceData;
 class Group;
 class InstanceSave;
 struct ScriptInfo;
-struct ScriptAction;
 class BattleGround;
 class GridMap;
 
@@ -80,7 +80,7 @@ enum LevelRequirementVsMode
 
 #define MIN_UNLOAD_DELAY      1                             // immediate unload
 
-class MANGOS_DLL_SPEC Map : public GridRefManager<NGridType>, public MaNGOS::ObjectLevelLockable<Map, ACE_Thread_Mutex>
+class MANGOS_DLL_SPEC Map : public GridRefManager<NGridType>
 {
     friend class MapReference;
     friend class ObjectGridLoader;
@@ -276,7 +276,6 @@ class MANGOS_DLL_SPEC Map : public GridRefManager<NGridType>, public MaNGOS::Obj
         void SendObjectUpdates();
         std::set<Object *> i_objectsToClientUpdate;
     protected:
-        typedef MaNGOS::ObjectLevelLockable<Map, ACE_Thread_Mutex>::Lock Guard;
 
         MapEntry const* i_mapEntry;
         uint8 i_spawnMode;
