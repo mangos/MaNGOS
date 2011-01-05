725d5953c11b67d35f0a65c12d9b5590fff7d0df
Daggy <daggy@getmangos.com>
2011-01-05 10:22:44 +0100

    [10967] Add dummy effect of spell 38194 and limit it's maxTargets
    Correct order of spellfamily case.
    Note: triggered spell 38203 needs target map fill
    
    Signed-off-by: NoFantasy <nofantasy@nf.no>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1517,6 +1517,18 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
             }
             break;
         }
+        case SPELLFAMILY_MAGE:
+        {
+            if (m_spellInfo->Id == 38194)                   // Blink
+                unMaxTargets = 1;
+            break;
+        }
+        case SPELLFAMILY_DRUID:
+        {
+            if (m_spellInfo->SpellFamilyFlags2 & 0x00000100)// Starfall
+                unMaxTargets = 2;
+            break;
+        }
         case SPELLFAMILY_PALADIN:
             if (m_spellInfo->Id == 20424)                   // Seal of Command (2 more target for single targeted spell)
             {
@@ -1529,12 +1541,6 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
                         EffectChainTarget = 0;              // no chain targets
             }
             break;
-        case SPELLFAMILY_DRUID:
-        {
-            if (m_spellInfo->SpellFamilyFlags2 & 0x00000100)// Starfall
-                unMaxTargets = 2;
-            break;
-        }
         default:
             break;
     }
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "10966"
+ #define REVISION_NR "10967"
 #endif // __REVISION_NR_H__
