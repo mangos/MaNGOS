59c2e45b552843c8a3ad0acee353227b7833f479
/dev/rsa <kris13@bk.ru>
2011-01-18 21:11:07 +0300

    Revert "[patch 205] Fixed a problem that make server crash at grid unloading. Patch provided by SilverIce"
    This reverts commit 52382da487dcf870b85aa890a5c5f50a9e717687.
    Try to fix crushes by Vinolentus research
    Conflicts:
    
    	src/game/Map.cpp

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -1052,8 +1052,7 @@ void Map::AddObjectToRemoveList(WorldObject *obj)
 {
     MANGOS_ASSERT(obj->GetMapId()==GetId() && obj->GetInstanceId()==GetInstanceId());
 
-    // need clean references at end of update cycle, NOT during it! called at Map::Remove
-    // obj->CleanupsBeforeDelete();                            // remove or simplify at least cross referenced links
+    obj->CleanupsBeforeDelete();                            // remove or simplify at least cross referenced links
 
     i_objectsToRemove.insert(obj);
     //DEBUG_LOG("Object (GUID: %u TypeId: %u ) added to removing list.",obj->GetGUIDLow(),obj->GetTypeId());
