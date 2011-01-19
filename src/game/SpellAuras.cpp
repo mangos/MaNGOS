a2d01c5961ba389645c870950791594f88a36d74
Feanordev <Feanordev@gmail.com>
2011-01-19 11:31:01 +0100

    Missing line to make Moonkin Form damage reduction in stuns

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6377,6 +6377,7 @@ void Aura::HandleShapeshiftBoosts(bool apply)
             break;
         case FORM_MOONKIN:
             spellId1 = 24905;
+            spellId2 = 69366;
             MasterShaperSpellId = 48421;
             break;
         case FORM_FLIGHT:
