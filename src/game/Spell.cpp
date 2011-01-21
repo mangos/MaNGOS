4928fdee634589a602e5c0a68fe67fc901a63d59
schmoozerd <schmoozerd@getmangos.com>
2011-01-21 10:51:59 +0100

    [11056] Limit max targets for spell 802/804
    Signed-off-by: NoFantasy <nofantasy@nf.no>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1483,6 +1483,8 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
         {
             switch(m_spellInfo->Id)
             {
+                case 802:                                   // Mutate Bug
+                case 804:                                   // Explode Bug
                 case 23138:                                 // Gate of Shazzrah
                 case 31347:                                 // Doom TODO: exclude top threat target from target selection
                 case 33711:                                 // Murmur's Touch
