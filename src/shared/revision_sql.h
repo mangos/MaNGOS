7e8e913dbf54b3fc765489486a001d0337e954af
Lynx3d <lynx3d@some-imaginary-isp.org>
2011-01-21 14:34:25 +0100

    [11058] Add 2P bonus of item set 894 and 4P bonus of item set 884 to spell_proc_event

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_10973_01_characters_game_event_status"
- #define REVISION_DB_MANGOS "required_11040_02_mangos_spell_bonus_data"
+ #define REVISION_DB_MANGOS "required_11058_01_mangos_spell_proc_event"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__
