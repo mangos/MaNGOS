61ef48a9cd9bc6521cf1cb8b73663034ea8f614a
/dev/rsa <kris13@bk.ru>
2011-01-27 13:33:22 +0300

    pets not autoattack friendly targets, causing damage to owner

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6037,8 +6037,16 @@ void Unit::AttackedBy(Unit *attacker)
         ((Creature*)this)->AI()->AttackedBy(attacker);
 
     // trigger pet AI reaction
-    if (Pet *pet = GetPet())
-        pet->AttackedBy(attacker);
+    if (attacker->IsHostileTo(this))
+    {
+        GroupPetList m_groupPets = GetPets();
+        if (!m_groupPets.empty())
+        {
+            for (GroupPetList::const_iterator itr = m_groupPets.begin(); itr != m_groupPets.end(); ++itr)
+                if (Pet* _pet = GetMap()->GetPet(*itr))
+                    _pet->AttackedBy(attacker);
+        }
+    }
 }
 
 bool Unit::AttackStop(bool targetSwitch /*=false*/)
