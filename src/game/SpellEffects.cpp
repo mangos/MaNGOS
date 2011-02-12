1800bc7f17706f2379299f306bfee4ba1838658b
NeatElves <NeatElves@getmangos.com>
2011-02-12 17:07:11 +0200

    [11148] Add dummy effect for spell [74904]
    Signed-off-by: Ambal <pogrebniak@gala.net>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1455,6 +1455,7 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                 case 51961:                                 // Captured Chicken Cover
                 case 55364:                                 // Create Ghoul Drool Cover
                 case 61832:                                 // Rifle the Bodies: Create Magehunter Personal Effects Cover
+                case 74904:                                 // Pickup Sen'jin Frog
                 {
                     if (!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT || m_caster->GetTypeId() != TYPEID_PLAYER)
                         return;
@@ -1470,6 +1471,7 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                         case 51961: spellId = 51037; break;
                         case 55364: spellId = 55363; break;
                         case 61832: spellId = 47096; break;
+                        case 74904: spellId = 74905; break;
                     }
 
                     if (const SpellEntry *pSpell = sSpellStore.LookupEntry(spellId))
