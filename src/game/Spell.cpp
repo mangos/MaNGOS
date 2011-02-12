83fd26005698488e18a73f2aed3f53126b8a31a9
VladimirMangos <vladimir@getmangos.com>
2011-02-12 21:56:38 +0300

    [11153] Apply at least same checks at used at hit for miss aggro.
    * Miss cases must have not less stricted checkd for combat start for comparison with normal hit.
    * Also cleanup code

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1010,18 +1010,21 @@ void Spell::DoAllEffectOnTarget(TargetInfo *target)
         if (target->reflectResult == SPELL_MISS_NONE)       // If reflected spell hit caster -> do all effect on him
             DoSpellHitOnUnit(m_caster, mask);
     }
-
-    if(missInfo == SPELL_MISS_MISS || missInfo == SPELL_MISS_RESIST)
+    else if(missInfo == SPELL_MISS_MISS || missInfo == SPELL_MISS_RESIST)
     {
-        Unit* realCaster = GetAffectiveCaster();
-        if(realCaster && realCaster != unit)
+        if(real_caster && real_caster != unit)
         {
-            if (!unit->isInCombat() && unit->GetTypeId() != TYPEID_PLAYER && ((Creature*)unit)->AI())
-                ((Creature*)unit)->AI()->AttackedBy(realCaster);
+            // can cause back attack (if detected)
+            if (!(m_spellInfo->AttributesEx3 & SPELL_ATTR_EX3_NO_INITIAL_AGGRO) && !IsPositiveSpell(m_spellInfo->Id) &&
+                m_caster->isVisibleForOrDetect(unit, unit, false))
+            {
+                if (!unit->isInCombat() && unit->GetTypeId() != TYPEID_PLAYER && ((Creature*)unit)->AI())
+                    ((Creature*)unit)->AI()->AttackedBy(real_caster);
 
-            unit->AddThreat(realCaster);
-            unit->SetInCombatWith(realCaster);
-            realCaster->SetInCombatWith(unit);
+                unit->AddThreat(real_caster);
+                unit->SetInCombatWith(real_caster);
+                real_caster->SetInCombatWith(unit);
+            }
         }
     }
 
