c5621a7a9523f8da54a013e3db8dc8cdc481d7fb
VladimirMangos <vladimir@getmangos.com>
2011-02-14 03:25:45 +0300

    Update git_id not generate by default sql/updates/Makefile.am
    TO DEVS: please update used git_id
    
    Now cmake support adding completed.
    
    * If you plan build MaNGOS at Unix/Linux then you need have
      installed cmake package and only can build mangos wiht cmake use:
      cmake; make; make install
    
    * If you plan build MaNGOS at Windows then for this time prefered
      use old way with Visual Studio projects in win directory.
    
      You _can_ use cmake way MaNGOS build but this experemental mode.
      For cmake using build you need have cmake tool installed at Windows.
      And need run cmake (for example by run createprojects.bat in MaNGOS
      root directory and then load generated Visual Studio solltion file
      build/mangos.sln

--- contrib/git_id/git_id.cpp
+++ contrib/git_id/git_id.cpp
@@ -92,7 +92,7 @@ bool local = false;
 bool do_fetch = false;
 bool do_sql = false;
 bool use_new_index = true;
-
+bool generate_makefile = false;                             // not need for cmake build systems
 // aux
 
 char origins[NUM_REMOTES][MAX_REMOTE];
@@ -910,7 +910,8 @@ int main(int argc, char *argv[])
     if(do_sql)
     {
         DO( convert_sql_updates()       );
-        DO( generate_sql_makefile()     );
+        if (generate_makefile)
+            DO( generate_sql_makefile() );
         DO( change_sql_database()       );
         DO( write_rev_sql()             );
     }
