e5e06cfc16b71e91a1685ce3c6ee460770e08bc3
Ambal <pogrebniak@gala.net>
2011-04-14 01:41:34 +0300

    [11347] More accurate calculation of next movement update time in RandomMovementGenerator::_setRandomLocation()
    Signed-off-by: Ambal <pogrebniak@gala.net>

--- src/game/RandomMovementGenerator.cpp
+++ src/game/RandomMovementGenerator.cpp
@@ -105,7 +105,7 @@ RandomMovementGenerator<Creature>::_setRandomLocation(Creature &creature)
     //else if (is_water_ok)                                 // Swimming mode to be done with more than this check
     else
     {
-        i_nextMoveTime.Reset(urand(500+i_destinationHolder.GetTotalTravelTime(), 10000+i_destinationHolder.GetTotalTravelTime()));
+        i_nextMoveTime.Reset(i_destinationHolder.GetTotalTravelTime() + urand(500, 10000));
         creature.AddSplineFlag(SPLINEFLAG_WALKMODE);
     }
 }
