d2665a14993dcaf5ada05ada47335a87e4d8f413
stfxpi <stfxpi@getmangos.com>
2011-05-13 15:04:59 +0400

    [11480] Avoid use 2 random call in dummy 14537
    For random generators use in same calculation 2 random roll calls can
    expose internal dependences in pseudo-random generation algo
    and in result generated values will not so random as expected.
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -901,11 +901,12 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                         spell_id = 11538;
                     else if (roll < 70)                     // Chain Lighting (20% chance)
                         spell_id = 21179;
-                    else if (roll < 80)                     // Polymorph (10% chance)
+                    else if (roll < 77)                     // Polymorph (10% chance, 7% to target)
+                        spell_id = 14621;
+                    else if (roll < 80)                     // Polymorph (10% chance, 3% to self, backfire)
                     {
                         spell_id = 14621;
-                        if (urand(0, 9) < 3)                // 30% chance to self-cast
-                            newTarget = m_caster;
+                        newTarget = m_caster;
                     }
                     else if (roll < 95)                     // Enveloping Winds (15% chance)
                         spell_id = 25189;
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "11479"
+ #define REVISION_NR "11480"
 #endif // __REVISION_NR_H__
