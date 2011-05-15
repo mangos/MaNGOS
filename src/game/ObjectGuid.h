348955476f144279fff9118d421d9133f0109e9a
VladimirMangos <vladimir@getmangos.com>
2011-05-15 23:34:43 +0400

    [11491] Step back to implicit ObjectGuid(uint64 const& guid) in wait code users updating.
    Must be reverted with drop GetGUID() when code ready.

--- src/game/ObjectGuid.h
+++ src/game/ObjectGuid.h
@@ -88,7 +88,7 @@ class MANGOS_DLL_SPEC ObjectGuid
 {
     public:                                                 // constructors
         ObjectGuid() : m_guid(0) {}
-        explicit ObjectGuid(uint64 const& guid) : m_guid(guid) {}
+        ObjectGuid(uint64 const& guid) : m_guid(guid) {}    // temporary allowed implicit cast, really bad in connection with operator uint64()
         ObjectGuid(HighGuid hi, uint32 entry, uint32 counter) : m_guid(counter ? uint64(counter) | (uint64(entry) << 24) | (uint64(hi) << 48) : 0) {}
         ObjectGuid(HighGuid hi, uint32 counter) : m_guid(counter ? uint64(counter) | (uint64(hi) << 48) : 0) {}
 
