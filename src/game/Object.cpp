f8bb6459af19f4b8e7e8a4d0b0ff7bc7b2445845
VladimirMangos <vladimir@getmangos.com>
2011-06-20 22:36:37 +0400

    [11658] Revert "[11298] NearUsedPosDo searcher should add only units with correct coordinates. Possibly can help with freezes when 'DetectPosCollision = 1'."
    This reverts commit a79ca96e4ae7b6ab1b70319e8b3011253068c841.
    
    This restore work object pos selector for units.
    Reverted commit make fully disabled it work for multiply attackers case.

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -1722,9 +1722,9 @@ namespace MaNGOS
                 {
                     x = c->GetPositionX();
                     y = c->GetPositionY();
-
-                    add(c,x,y);
                 }
+
+                add(c,x,y);
             }
 
             template<class T>
