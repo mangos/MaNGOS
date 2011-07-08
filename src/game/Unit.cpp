d28fc6d8ef78defaafc3ca23c132efb9af79d54e
SilverIce <slifeleaf@gmail.com>
2011-07-08 03:58:38 +0300

    [11719] Destroy all movement generators at Unit::CleanupsBeforeDelete call
    this fixes possible crash at grid unloading

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9448,7 +9448,7 @@ void Unit::CleanupsBeforeDelete()
         else
             getHostileRefManager().deleteReferences();
         RemoveAllAuras(AURA_REMOVE_BY_DELETE);
-        GetMotionMaster()->Clear(false);                    // remove different non-standard movement generators.
+        GetMotionMaster()->Clear(false,true);         // remove all movement generators.           
     }
     WorldObject::CleanupsBeforeDelete();
 }
