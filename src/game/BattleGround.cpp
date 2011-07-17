de8599373b84e380fdc55b57d6b5cc908bef26b0
VladimirMangos <vladimir@getmangos.com>
2011-07-17 05:23:44 +0400

    [11741] Drop recently added duplicate code with remove SPELL_AURA_SPIRIT_OF_REDEMPTION

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -1011,10 +1011,6 @@ void BattleGround::RemovePlayerAtLeave(ObjectGuid guid, bool Transport, bool Sen
 
     Player *plr = sObjectMgr.GetPlayer(guid);
 
-    // should remove spirit of redemption
-    if (plr && plr->HasAuraType(SPELL_AURA_SPIRIT_OF_REDEMPTION))
-        plr->RemoveSpellsCausingAura(SPELL_AURA_MOD_SHAPESHIFT);
-
     if (plr)
     {
         // should remove spirit of redemption
