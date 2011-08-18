96b2cf6df9e93ad974c8fcbbdc0faf252b0477d6
kid 10 <kid_10@gmx.at>
2011-08-18 22:02:48 +0200

    [11798] Implement select spellDifficulty support in RemoveAurasDueToSpell
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4469,6 +4469,11 @@ void Unit::RemoveAuraHolderFromStack(uint32 spellId, uint32 stackAmount, ObjectG
 
 void Unit::RemoveAurasDueToSpell(uint32 spellId, SpellAuraHolder* except, AuraRemoveMode mode)
 {
+    SpellEntry const* spellEntry = sSpellStore.LookupEntry(spellId);
+    if (spellEntry && spellEntry->SpellDifficultyId && IsInWorld() && GetMap()->IsDungeon())
+        if (SpellEntry const* spellDiffEntry = GetSpellEntryByDifficulty(spellEntry->SpellDifficultyId, GetMap()->GetDifficulty(), GetMap()->IsRaid()))
+            spellId = spellDiffEntry->Id;
+
     SpellAuraHolderBounds bounds = GetSpellAuraHolderBounds(spellId);
     for (SpellAuraHolderMap::iterator iter = bounds.first; iter != bounds.second; )
     {
