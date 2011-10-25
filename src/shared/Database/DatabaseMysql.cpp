abb0c074433647b1071479384d82acc9bd9045ed
Vinolentus <655ff4c91d8e57f5@gmail.com>
2011-10-25 20:39:43 +0300

    [11835] Fix SqlStatement::addBool
    MYSQL_TYPE_BIT is not one of the allowable input values, so we have to pass it using an integer variable
    
    Signed-off-by: SilverIce <slifeleaf@gmail.com>

--- src/shared/Database/DatabaseMysql.cpp
+++ src/shared/Database/DatabaseMysql.cpp
@@ -481,9 +481,10 @@ enum_field_types MySqlPreparedStatement::ToMySQLType( const SqlStmtFieldData &da
     switch (data.type())
     {
     case FIELD_NONE:    dataType = MYSQL_TYPE_NULL;                     break;
-    case FIELD_BOOL:    dataType = MYSQL_TYPE_BIT;      bUnsigned = 1;  break;
-    case FIELD_I8:      dataType = MYSQL_TYPE_TINY;                     break;
+    // MySQL does not support MYSQL_TYPE_BIT as input type
+    case FIELD_BOOL:    //dataType = MYSQL_TYPE_BIT;      bUnsigned = 1;  break;
     case FIELD_UI8:     dataType = MYSQL_TYPE_TINY;     bUnsigned = 1;  break;
+    case FIELD_I8:      dataType = MYSQL_TYPE_TINY;                     break;
     case FIELD_I16:     dataType = MYSQL_TYPE_SHORT;                    break;
     case FIELD_UI16:    dataType = MYSQL_TYPE_SHORT;    bUnsigned = 1;  break;
     case FIELD_I32:     dataType = MYSQL_TYPE_LONG;                     break;
