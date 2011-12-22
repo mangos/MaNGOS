b3a23acf7a70a41ad36be2705e79463edf153b3e
stfx <stfx@hotmail.de>
2011-12-22 12:22:42 +0100

    [11874] Fix comment about PlayerUnderwaterState

--- src/game/Player.h
+++ src/game/Player.h
@@ -68,7 +68,7 @@ enum SpellModType
     SPELLMOD_PCT          = 108                             // SPELL_AURA_ADD_PCT_MODIFIER
 };
 
-// 2^n values, Player::m_isunderwater is a bitmask. These are mangos internal values, they are never send to any client
+// 2^n internal values, they are never sent to the client
 enum PlayerUnderwaterState
 {
     UNDERWATER_NONE                     = 0x00,
