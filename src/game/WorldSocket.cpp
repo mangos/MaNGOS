a9a971511b9c98089112a50fea5bb57a4ab3bfd9
DasBlub <DasBlub@gmail.com>
2012-01-02 18:18:46 +0100

    fix typo in comment

--- src/game/WorldSocket.cpp
+++ src/game/WorldSocket.cpp
@@ -702,7 +702,7 @@ int WorldSocket::ProcessIncoming (WorldPacket* new_pct)
                 {
                     // OK ,give the packet to WorldSession
                     aptr.release ();
-                    // WARNINIG here we call it with locks held.
+                    // WARNING here we call it with locks held.
                     // Its possible to cause deadlock if QueuePacket calls back
                     m_Session->QueuePacket (new_pct);
                     return 0;
