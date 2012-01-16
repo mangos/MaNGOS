1e35033c12a6f506a865ddbba5d936c107ce0d1a
Schmoozerd <schmoozerd@scriptdev2.com>
2012-01-16 17:19:35 +0100

    [11875] Update Copyright notice to year 2012
    Start timemachine and a Happy new year to all!

--- src/bindings/universal/config.h
+++ src/bindings/universal/config.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2011 MaNGOS <http://www.mangosproject.org/>
+ * Copyright (C) 2005-2012 MaNGOS <http://www.mangosproject.org/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
