b0be386d80f6fb50071f7225509326fac255db4b
Astoria <astoria@getmangos.com>
2012-01-25 21:20:54 +0100

    [11885] Add spell_proc_event values for spell 72413
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "11884"
+ #define REVISION_NR "11885"
 #endif // __REVISION_NR_H__
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_11785_02_characters_instance"
- #define REVISION_DB_MANGOS "required_11876_01_mangos_creature_linking_template"
+ #define REVISION_DB_MANGOS "required_11885_01_mangos_spell_proc_event"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__
