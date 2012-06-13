4fc77a78a66678ad6ebfdd9b52043d06be45cedf
TOM_RUS <tom_rus@getmangos.com>
2012-06-13 16:36:26 +0400

    Fixed typo.

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -933,9 +933,9 @@ void WorldSession::SendRedirectClient(std::string& ip, uint16 port)
     pkt << uint32(ip2);                                     // inet_addr(ipstr)
     pkt << uint16(port);                                    // port
 
-    pkt << uint32(GetLatency());                            // latency-related?
+    pkt << uint32(0);                                       // unknown
 
-    HMACSHA1 sha1(20, m_Socket->GetSessionKey().AsByteArray());
+    HMACSHA1 sha1(40, m_Socket->GetSessionKey().AsByteArray());
     sha1.UpdateData((uint8*)&ip2, 4);
     sha1.UpdateData((uint8*)&port, 2);
     sha1.Finalize();
