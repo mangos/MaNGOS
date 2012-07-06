c5e609c485ea90a19ca7f9f947b3c96c57d19327
Xfurry <xfurry@scriptdev2.com>
2012-07-06 14:44:47 +0200

    [12061] Assert correct loading of GO_TYPE_CAPTURE_POINT
    This will assert controlled behaviour when loading a `gameobject_template` entry of type Capture Point with bad data
    Patch improved by stfx
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -5937,6 +5937,18 @@ inline void CheckGOConsumable(GameObjectInfo const* goInfo,uint32 dataN,uint32 N
         goInfo->id,goInfo->type,N,dataN);
 }
 
+inline void CheckAndFixGOCaptureMinTime(GameObjectInfo const* goInfo, uint32 const& dataN, uint32 N)
+{
+    if (dataN > 0)
+        return;
+
+    sLog.outErrorDb("Gameobject (Entry: %u GoType: %u) has data%d=%u but minTime field value must be > 0.",
+        goInfo->id, goInfo->type, N, dataN);
+
+    // prevent division through 0 exception
+    const_cast<uint32&>(dataN) = 1;
+}
+
 void ObjectMgr::LoadGameobjectInfo()
 {
     SQLGameObjectLoader loader;
@@ -6097,6 +6109,11 @@ void ObjectMgr::LoadGameobjectInfo()
                 if (goInfo->flagdrop.lockId)
                     CheckGOLockId(goInfo,goInfo->flagdrop.lockId,0);
                 CheckGONoDamageImmuneId(goInfo,goInfo->flagdrop.noDamageImmune,3);
+                break;
+            }
+            case GAMEOBJECT_TYPE_CAPTURE_POINT:             //29
+            {
+                CheckAndFixGOCaptureMinTime(goInfo, goInfo->capturePoint.minTime, 16);
                 break;
             }
             case GAMEOBJECT_TYPE_BARBER_CHAIR:              //32
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12060"
+ #define REVISION_NR "12061"
 #endif // __REVISION_NR_H__
