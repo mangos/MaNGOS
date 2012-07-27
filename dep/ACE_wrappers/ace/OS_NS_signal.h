ef79a25c34a24512a6bc3e231b70133b74aea5d7
antiroot <antiroot@geekspark.com>
2012-07-27 10:43:03 -0500

    [12072] Apply ACE patches for FreeBSD

--- dep/ACE_wrappers/ace/OS_NS_signal.h
+++ dep/ACE_wrappers/ace/OS_NS_signal.h
@@ -33,10 +33,10 @@
 #endif
 #define ACE_EXPORT_MACRO ACE_Export
 
-#if defined (__Lynx__)
-// LynxOS defines pthread_sigmask() in pthread.h
+#if defined (__Lynx__) || defined (__OpenBSD__) || defined (__FreeBSD__)
+// LynxOS, OpenBSD and FreeBSD define pthread_sigmask() in pthread.h
 # include "ace/os_include/os_pthread.h"
-#endif /* __Lynx__ */
+#endif /* __Lynx__ || OpenBSD || FreeBSD */
 
 /*
  * We inline and undef some functions that may be implemented
