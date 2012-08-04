11f12c2ed436ca90a695d930411e18a33f50001c
bugeyedcreepy <scott@spartacle.com.au>
2012-08-05 09:49:47 +1000

    Update sql/updates/0.15/8965_01_mangos_mangos_string.sql
    Server Chat: Make Sense Damn You!

--- sql/updates/0.15/8965_01_mangos_mangos_string.sql
+++ sql/updates/0.15/8965_01_mangos_mangos_string.sql
@@ -3,4 +3,4 @@ ALTER TABLE db_version CHANGE COLUMN required_8950_01_mangos_spell_proc_event re
 DELETE FROM mangos_string WHERE entry in (1015);
 
 INSERT INTO mangos_string VALUES
- (1015,'Used not fully typed quit command, need type it fully (quit), or command used not in RA command line.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+ (1015,'Can only quit a Remote Admin console or the quit command was not entered in full (quit).',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
