3600bd504d5eb6dd11d129dd9250a4295357ac7c
bugeyedcreepy <scott@spartacle.com.au>
2012-08-05 10:12:08 +1000

    Update sql/updates/0.16/09767_01_mangos_mangos_string.sql
    Server Chat: Make Sense Damn You!

--- sql/updates/0.16/09767_01_mangos_mangos_string.sql
+++ sql/updates/0.16/09767_01_mangos_mangos_string.sql
@@ -8,8 +8,8 @@ INSERT INTO mangos_string VALUES
 (1019, 'No characters found.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1020, 'Restoring the following characters:',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1021, 'Deleting the following characters:',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(1022, 'ERROR: You can only assign a new name if you have only selected a single character!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(1023, 'Character \'%s\' (GUID: %u Account %u) can\'t be restored: account not exist!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1022, 'ERROR: You can only assign a new name for a single selected character!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1023, 'Character \'%s\' (GUID: %u Account %u) can\'t be restored: account doesn\'t exist!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1024, 'Character \'%s\' (GUID: %u Account %u) can\'t be restored: account character list full!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1025, 'Character \'%s\' (GUID: %u Account %u) can\'t be restored: new name already used!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1026, 'GUID: %u Name: %s Account: %s (%u) Date: %s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
