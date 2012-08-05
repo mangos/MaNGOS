cdd23448d4879fe309b1eec7db00f62e0bb212f9
bugeyedcreepy <scott@spartacle.com.au>
2012-08-05 10:36:19 +1000

    Update sql/updates/11337_01_mangos_mangos_string.sql
    Server Chat: Make Sense Damn You!

--- sql/updates/11337_01_mangos_mangos_string.sql
+++ sql/updates/11337_01_mangos_mangos_string.sql
@@ -3,4 +3,4 @@ ALTER TABLE db_version CHANGE COLUMN required_11335_02_mangos_mangos_string requ
 DELETE FROM mangos_string WHERE entry IN (1503);
 
 INSERT INTO mangos_string VALUES
-(1503,'Can not add spawn because no free guids for static spawn in reserved guids range. Server restart is required before command can be used. Also look GuidReserveSize.* config options.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+(1503,'Cannot add spawn because no free guids for static spawn in reserved guids range. Server restart is required before command can be used. Also look GuidReserveSize.* config options.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
