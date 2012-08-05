ef8ef1297bfc6ffb4713ec7ddcabe313416147f0
bugeyedcreepy <scott@spartacle.com.au>
2012-08-05 10:28:57 +1000

    Update sql/mangos.sql
    Server Chat: Make Sense Damn You!

--- sql/mangos.sql
+++ sql/mangos.sql
@@ -3974,7 +3974,7 @@ INSERT INTO `mangos_string` VALUES
 (1110,'%d%s - %s X:%f Y:%f Z:%f MapId:%d',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1111,'%d%s - %s X:%f Y:%f Z:%f MapId:%d',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1112,'Failed to open file: %s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(1113,'Account %s (%u) has max amount allowed characters (client limit)',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1113,'Account %s (%u) has the maximum characters allowable (client limit)',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1114,'Dump file have broken data!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1115,'Invalid character name!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1116,'Invalid character guid!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
