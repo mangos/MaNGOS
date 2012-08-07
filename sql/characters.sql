a40eeda604905276178c885f7b64fc31984645e5
Salja <salja2012@hotmail.de>
2012-08-07 09:41:20 +0200

    [0003] Typo
    Signed-off-by: Salja <salja2012@hotmail.de>

--- sql/characters.sql
+++ sql/characters.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `character_db_version`;
 CREATE TABLE `character_db_version` (
-  `required_0001_01_characters` bit(1) default NULL
+  `required_0001_xxxxx_01_characters` bit(1) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
 
 --
