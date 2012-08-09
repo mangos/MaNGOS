6fbd3ef61260ac16cebf8267f425ff5b19710e9b
Salja <salja2012@hotmail.de>
2012-08-09 15:49:58 +0200

    [0038] player_classlevelstats is droped in [0023]
    Signed-off-by: Salja <salja2012@hotmail.de>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "0037"
+ #define REVISION_NR "0038"
 #endif // __REVISION_NR_H__
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
 #define REVISION_DB_CHARACTERS "required_0028_xxxxx_01_characters_character_phase_data"
-#define REVISION_DB_MANGOS "required_0034_xxxxx_03_mangos_player_classlevelstats"
+#define REVISION_DB_MANGOS "required_0038_xxxxx_01_mangos_player_classlevelstats"
 #define REVISION_DB_REALMD "required_0014_xxxxx_01_realmd_account_access"
 #endif // __REVISION_SQL_H__
