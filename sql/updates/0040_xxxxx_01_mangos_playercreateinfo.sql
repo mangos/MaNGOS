d24b51b0fd9ef9a5fb42d348a5e44bd4b8351f58
Salja <salja2012@hotmail.de>
2012-08-09 17:08:46 +0200

    [0040] Some update for playercreateinfo.
    Signed-off-by: Salja <salja2012@hotmail.de>

---
+++ sql/updates/0040_xxxxx_01_mangos_playercreateinfo.sql
@@ -0,0 +1,3 @@
+ALTER TABLE db_version CHANGE COLUMN required_0038_xxxxx_01_mangos_player_classlevelstats required_0040_xxxxx_01_mangos_playercreateinfo bit;
+
+UPDATE playercreateinfo SET phaseMap=638 WHERE race=22;
