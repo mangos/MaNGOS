f250371e70ffd9c6b96acc2d9a7c76a555d76b74
Salja <salja2012@hotmail.de>
2012-08-15 19:17:06 +0200

    [0099.99999] Typoooooo Grrrr
    Signed-off-by: Salja <salja2012@hotmail.de>

--- sql/characters.sql
+++ sql/characters.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `character_db_version`;
 CREATE TABLE `character_db_version` (
-  `required_0096_xxxxx_01_characters_character_phase_data` bit(1) default NULL
+  `required_0099_xxxxx_01_characters_character_phase_data` bit(1) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
 
 --
