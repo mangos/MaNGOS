3964df8e9145b65c47a97852552aee120e3105e7
stfx <stfx@hotmail.de>
2012-08-17 14:06:32 +0200

    Add OutdoorPvP class

--- src/game/OutdoorPvP/OutdoorPvP.cpp
+++ src/game/OutdoorPvP/OutdoorPvP.cpp
@@ -15,3 +15,149 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
+
+#include "OutdoorPvP.h"
+#include "../Language.h"
+#include "../World.h"
+#include "../ObjectMgr.h"
+#include "../Object.h"
+#include "../GameObject.h"
+#include "../Player.h"
+
+/**
+   Function that adds a player to the players of the affected outdoor pvp zones
+
+   @param   player to add
+   @param   whether zone is main outdoor pvp zone or a affected zone
+ */
+void OutdoorPvP::HandlePlayerEnterZone(Player* player, bool isMainZone)
+{
+    m_zonePlayers[player->GetObjectGuid()] = isMainZone;
+}
+
+/**
+   Function that removes a player from the players of the affected outdoor pvp zones
+
+   @param   player to remove
+   @param   whether zone is main outdoor pvp zone or a affected zone
+ */
+void OutdoorPvP::HandlePlayerLeaveZone(Player* player, bool isMainZone)
+{
+    if (m_zonePlayers.erase(player->GetObjectGuid()))
+    {
+        // remove the world state information from the player
+        if (isMainZone && !player->GetSession()->PlayerLogout())
+            SendRemoveWorldStates(player);
+
+        sLog.outDebug("Player %s left an Outdoor PvP zone", player->GetName());
+    }
+}
+
+/**
+   Function that updates the world state for all the players of the outdoor pvp zone
+
+   @param   world state to update
+   @param   new world state value
+ */
+void OutdoorPvP::SendUpdateWorldState(uint32 field, uint32 value)
+{
+    for (GuidZoneMap::iterator itr = m_zonePlayers.begin(); itr != m_zonePlayers.end(); ++itr)
+    {
+        // only send world state update to main zone
+        if (!itr->second)
+            continue;
+
+        if (Player* player = sObjectMgr.GetPlayer(itr->first))
+            player->SendUpdateWorldState(field, value);
+    }
+}
+
+/**
+   Function that handles player kills in the main outdoor pvp zones
+
+   @param   player who killed another player
+   @param   victim who was killed
+ */
+void OutdoorPvP::HandlePlayerKill(Player* killer, Unit* victim)
+{
+    if (Group* group = killer->GetGroup())
+    {
+        for (GroupReference* itr = group->GetFirstMember(); itr != NULL; itr = itr->next())
+        {
+            Player* groupMember = itr->getSource();
+
+            if (!groupMember)
+                continue;
+
+            // skip if too far away
+            if (!groupMember->IsAtGroupRewardDistance(victim))
+                continue;
+
+            // creature kills must be notified, even if not inside objective / not outdoor pvp active
+            // player kills only count if active and inside objective
+            if (groupMember->CanUseOutdoorCapturePoint())
+                HandlePlayerKillInsideArea(groupMember, victim);
+        }
+    }
+    else
+    {
+        // creature kills must be notified, even if not inside objective / not outdoor pvp active
+        if (killer && killer->CanUseOutdoorCapturePoint())
+            HandlePlayerKillInsideArea(killer, victim);
+    }
+}
+
+// apply a team buff for the main and affected zones
+void OutdoorPvP::BuffTeam(Team team, uint32 spellId, bool remove /*= false*/)
+{
+    for (GuidZoneMap::iterator itr = m_zonePlayers.begin(); itr != m_zonePlayers.end(); ++itr)
+    {
+        Player* player = sObjectMgr.GetPlayer(itr->first);
+        if (player && player->GetTeam() == team)
+        {
+            if (remove)
+                player->RemoveAurasDueToSpell(spellId);
+            else
+                player->CastSpell(player, spellId, true);
+        }
+    }
+}
+
+uint32 OutdoorPvP::GetBannerArtKit(Team team, uint32 artKitAlliance /*= CAPTURE_ARTKIT_ALLIANCE*/, uint32 artKitHorde /*= CAPTURE_ARTKIT_HORDE*/, uint32 artKitNeutral /*= CAPTURE_ARTKIT_NEUTRAL*/)
+{
+    switch (team)
+    {
+        case ALLIANCE:
+            return artKitAlliance;
+        case HORDE:
+            return artKitHorde;
+        default:
+            return artKitNeutral;
+    }
+}
+
+void OutdoorPvP::SetBannerVisual(const WorldObject* objRef, ObjectGuid goGuid, uint32 artKit, uint32 animId)
+{
+    if (GameObject* go = objRef->GetMap()->GetGameObject(goGuid))
+        SetBannerVisual(go, artKit, animId);
+}
+
+void OutdoorPvP::SetBannerVisual(GameObject* go, uint32 artKit, uint32 animId)
+{
+    go->SendGameObjectCustomAnim(go->GetObjectGuid(), animId);
+    go->SetGoArtKit(artKit);
+    go->Refresh();
+}
+
+void OutdoorPvP::RespawnGO(const WorldObject* objRef, ObjectGuid goGuid, bool respawn)
+{
+    if (GameObject* go = objRef->GetMap()->GetGameObject(goGuid))
+    {
+        go->SetRespawnTime(7 * DAY);
+
+        if (respawn)
+            go->Refresh();
+        else if (go->isSpawned())
+            go->SetLootState(GO_JUST_DEACTIVATED);
+    }
+}
--- src/game/OutdoorPvP/OutdoorPvP.h
+++ src/game/OutdoorPvP/OutdoorPvP.h
@@ -19,4 +19,103 @@
 #ifndef OUTDOOR_PVP_H
 #define OUTDOOR_PVP_H
 
+#include "Common.h"
+#include "../ObjectGuid.h"
+#include "../SharedDefines.h"
+#include "OutdoorPvPMgr.h"
+
+class WorldPacket;
+class WorldObject;
+class Player;
+class GameObject;
+class Unit;
+class Creature;
+
+enum CapturePointArtKits
+{
+    CAPTURE_ARTKIT_ALLIANCE = 2,
+    CAPTURE_ARTKIT_HORDE    = 1,
+    CAPTURE_ARTKIT_NEUTRAL  = 21
+};
+
+enum CapturePointAnimations
+{
+    CAPTURE_ANIM_ALLIANCE   = 1,
+    CAPTURE_ANIM_HORDE      = 0,
+    CAPTURE_ANIM_NEUTRAL    = 2
+};
+
+typedef std::map<ObjectGuid /*playerGuid*/, bool /*isMainZone*/> GuidZoneMap;
+
+class OutdoorPvP
+{
+    friend class OutdoorPvPMgr;
+
+    public:
+        OutdoorPvP() {}
+        ~OutdoorPvP() {}
+
+        // called when the zone is initialized
+        virtual void FillInitialWorldStates(WorldPacket& /*data*/, uint32& /*count*/) {}
+
+        // Process Capture event
+        virtual bool HandleEvent(uint32 /*eventId*/, GameObject* /*go*/) { return false; }
+
+        // handle capture objective complete
+        virtual void HandleObjectiveComplete(uint32 /*eventId*/, std::list<Player*> /*players*/, Team /*team*/) {}
+
+        // Called when a creature or gameobject is created
+        virtual void HandleCreatureCreate(Creature* /*creature*/) {}
+        virtual void HandleGameObjectCreate(GameObject* /*go*/) {}
+
+        // Called on creature death
+        virtual void HandleCreatureDeath(Creature* /*creature*/) {}
+
+        // called when a player uses a gameobject related to outdoor pvp events
+        virtual bool HandleGameObjectUse(Player* /*player*/, GameObject* /*go*/) { return false; }
+
+        // called when a player triggers an areatrigger
+        virtual bool HandleAreaTrigger(Player* /*player*/, uint32 /*triggerId*/) { return false; }
+
+        // called when a player drops a flag
+        virtual bool HandleDropFlag(Player* /*player*/, uint32 /*spellId*/) { return false; }
+
+        // update - called by the OutdoorPvPMgr
+        virtual void Update(uint32 /*diff*/) {}
+
+        // handle npc/player kill
+        void HandlePlayerKill(Player* killer, Unit* victim);
+
+    protected:
+
+        // Player related stuff
+        virtual void HandlePlayerEnterZone(Player* /*player*/, bool /*isMainZone*/);
+        virtual void HandlePlayerLeaveZone(Player* /*player*/, bool /*isMainZone*/);
+
+        // remove world states
+        virtual void SendRemoveWorldStates(Player* /*player*/) {}
+
+        // handle npc/player kill
+        virtual void HandlePlayerKillInsideArea(Player* /*killer*/, Unit* /*victim*/) {}
+
+        // send world state update to all players present
+        void SendUpdateWorldState(uint32 field, uint32 value);
+
+        // applies buff to a team inside the specific zone
+        void BuffTeam(Team team, uint32 spellId, bool remove = false);
+
+        // get banner artkit based on controlling team
+        uint32 GetBannerArtKit(Team team, uint32 artKitAlliance = CAPTURE_ARTKIT_ALLIANCE, uint32 artKitHorde = CAPTURE_ARTKIT_HORDE, uint32 artKitNeutral = CAPTURE_ARTKIT_NEUTRAL);
+
+        // set banner visual
+        void SetBannerVisual(const WorldObject* objRef, ObjectGuid goGuid, uint32 artKit, uint32 animId);
+        void SetBannerVisual(GameObject* go, uint32 artKit, uint32 animId);
+
+        // Handle gameobject spawn / despawn
+        void RespawnGO(const WorldObject* objRef, ObjectGuid goGuid, bool respawn);
+
+        // store the players inside the area
+        GuidZoneMap m_zonePlayers;
+};
+
 #endif
