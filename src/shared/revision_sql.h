73f7dd4a899ceb7490e1b9c814f400313bdd3abb
crackm <crackm@getmangos.com>
2012-08-17 16:15:03 +0200

    [0123] Add some visual server side spells

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_0099_xxxxx_01_characters_character_phase_data"
- #define REVISION_DB_MANGOS "required_0122_xxxxx_01_mangos_item_template"
+ #define REVISION_DB_MANGOS "required_0123_12093_01_mangos_spell_template"
  #define REVISION_DB_REALMD "required_0014_xxxxx_01_realmd_account_access"
 #endif // __REVISION_SQL_H__
