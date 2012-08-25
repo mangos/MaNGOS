8a4c7b785778f9ec377bfe825d63da4f8404ff43
Salja <salja2012@hotmail.de>
2012-08-26 00:03:29 +0200

    [0185] Fix Typo/Crash Thanks Sar777 for fixing.
    Signed-off-by: Salja <salja2012@hotmail.de>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -524,7 +524,7 @@ void Spell::FillTargetMap()
         for (int j = 0; j < i; ++j)
         {
             SpellEffectEntry const* spellEffect1 = m_spellInfo->GetSpellEffect(SpellEffectIndex(j));
-            if (!spellEffect)
+            if (!spellEffect1)
                 continue;
 
             // Check if same target, but handle i.e. AreaAuras different
