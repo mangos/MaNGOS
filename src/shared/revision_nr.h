6515f1586afbc935df59df001686d0d2edb43fd6
Salja <salja2012@hotmail.de>
2012-09-05 16:47:18 +0200

    [12185] Update old Start->Items and added all new Start->Items
    Signed-off-by: Salja <salja2012@hotmail.de>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12184"
+ #define REVISION_NR "12185"
 #endif // __REVISION_NR_H__
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_12161_01_characters_characters"
- #define REVISION_DB_MANGOS "required_12180_01_mangos_spell_learn_spell"
+ #define REVISION_DB_MANGOS "required_12185_01_mangos_item_template"
  #define REVISION_DB_REALMD "required_12112_01_realmd_account_access"
 #endif // __REVISION_SQL_H__
