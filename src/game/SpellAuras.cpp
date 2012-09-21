41ee3cc173ba3795f5ecfca78f9fd24a0912f517
Sergey <Sergey.Chelombitko@gmail.com>
2012-09-21 16:31:38 +0600

    Update src/game/SpellAuras.cpp

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1588,7 +1588,7 @@ void Aura::TriggerSpell()
                         else
                             newAngle -= 2*M_PI_F/100;
 
-                        MapManager::NormalizeOrientation(newAngle);
+                        newAngle = MapManager::NormalizeOrientation(newAngle);
 
                         target->SetFacingTo(newAngle);
 
