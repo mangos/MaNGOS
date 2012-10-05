099df231de75bd65b292a3ee69d898583949c9f1
Schmoozerd <schmoozerd@scriptdev2.com>
2012-10-05 22:06:36 +0200

    Forgoten change from last commit

--- src/game/PathFinder.cpp
+++ src/game/PathFinder.cpp
@@ -181,9 +181,9 @@ void PathFinder::BuildPolyPath(const Vector3& startPos, const Vector3& endPos)
         DEBUG_FILTER_LOG(LOG_FILTER_PATHFINDING, "++ BuildPolyPath :: (startPoly == 0 || endPoly == 0)\n");
         BuildShortcut();
 
-        // Check for swimming or flying shortcut
         if (m_sourceUnit->GetTypeId() == TYPEID_UNIT)
         {
+            // Check for swimming or flying shortcut
             if ((startPoly == INVALID_POLYREF && m_sourceUnit->GetTerrain()->IsUnderWater(startPos.x, startPos.y, startPos.z)) ||
                     (endPoly == INVALID_POLYREF && m_sourceUnit->GetTerrain()->IsUnderWater(endPos.x, endPos.y, endPos.z)))
                 m_type = ((Creature*)m_sourceUnit)->CanSwim() ? PathType(PATHFIND_NORMAL | PATHFIND_NOT_USING_PATH) : PATHFIND_NOPATH;
