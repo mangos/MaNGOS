814a9b7efc26da9fb5f7fc3773bd50b8a954717f
Sergey <Sergey.Chelombitko@gmail.com>
2012-10-24 15:28:43 +0600

    Fix typo

--- src/game/movement/MoveSpline.cpp
+++ src/game/movement/MoveSpline.cpp
@@ -202,7 +202,7 @@ namespace Movement
 #define CHECK(exp) \
     if (!(exp))\
     {\
-        sLog.outError("MoveSplineInitArgs::Validate: expression '%s' failed for $s", #exp, unit->GetGuidStr().c_str());\
+        sLog.outError("MoveSplineInitArgs::Validate: expression '%s' failed for %s", #exp, unit->GetGuidStr().c_str());\
         return false;\
     }
         CHECK(path.size() > 1);
